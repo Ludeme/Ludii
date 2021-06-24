bd978b58eda14370953bd5ea5e2e9e948b01407b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2021-06-24 11:18:15 +0200

    Pachesi added

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Sik"))
+			if (fileEntry.getName().contains("Pachesi"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
