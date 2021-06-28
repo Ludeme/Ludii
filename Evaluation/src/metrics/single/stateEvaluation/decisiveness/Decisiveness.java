13ba7f56c72679c7fcb2bc96c767197969cdee7c
matthew <matthew@matthew-ThinkPad-X1-Extreme-Gen-3>
2021-06-28 13:23:15 +0200

    decisiveness added

---
+++ Evaluation/src/metrics/single/stateEvaluation/decisiveness/Decisiveness.java
@@ -0,0 +1,92 @@
+package metrics.single.stateEvaluation.decisiveness;
+
+import java.util.ArrayList;
+
+import org.apache.commons.rng.RandomProviderState;
+
+import game.Game;
+import metrics.Metric;
+import metrics.Utils;
+import other.concept.Concept;
+import other.context.Context;
+import other.trial.Trial;
+
+/**
+ * Number of turns after a winning player has a state evaluation above the decisiveness threshold.
+ * 
+ * @author matthew.stephenson
+ */
+public class Decisiveness extends Metric
+{
+
+	//-------------------------------------------------------------------------
+
+	/**
+	 * Constructor
+	 */
+	public Decisiveness()
+	{
+		super
+		(
+			"Decisiveness", 
+			"Number of turns after a winning player has a state evaluation above the decisiveness threshold.", 
+			"Core Ludii metric.", 
+			MetricType.OUTCOMES, 
+			0.0, 
+			1.0,
+			0.0,
+			Concept.Timeouts
+		);
+	}
+	
+	//-------------------------------------------------------------------------
+	
+	@Override
+	public double apply
+	(
+			final Game game,
+			final Trial[] trials,
+			final RandomProviderState[] randomProviderStates
+	)
+	{
+		double avgDecisivenessThreshold = 0.0;
+		for (int trialIndex = 0; trialIndex < trials.length; trialIndex++)
+		{
+			// Get trial and RNG information
+			final Trial trial = trials[trialIndex];
+			final RandomProviderState rngState = randomProviderStates[trialIndex];
+			
+			final double decisivenessThreshold = DecisivenessThreshold.decisivenessThreshold(game, trial, rngState);
+			
+			final Context context = Utils.setupNewContext(game, rngState);
+			final ArrayList<Integer> highestRankedPlayers = Utils.highestRankedPlayers(trial, context);
+			
+			int turnAboveDecisivenessthreshold = trial.numberRealMoves();
+			boolean aboveThresholdFound = false;
+			for (int i = trial.numInitialPlacementMoves(); i < trial.numMoves(); i++)
+			{
+				for (final Integer playerIndex : highestRankedPlayers)
+				{
+					if (Utils.UCTEvaluateState(context, playerIndex) > decisivenessThreshold)
+					{
+						aboveThresholdFound = true;
+						turnAboveDecisivenessthreshold = i - trial.numInitialPlacementMoves();
+						break;
+					}
+				}
+				
+				if (aboveThresholdFound)
+					break;
+				
+				context.game().apply(context, trial.getMove(i));
+			}
+			
+			avgDecisivenessThreshold += turnAboveDecisivenessthreshold/trial.numberRealMoves();
+		}
+
+		return avgDecisivenessThreshold / trials.length;
+	}
+
+	//-------------------------------------------------------------------------
+
+}
--- Evaluation/src/metrics/single/stateEvaluation/decisiveness/DecisivenessThreshold.java
+++ Evaluation/src/metrics/single/stateEvaluation/decisiveness/DecisivenessThreshold.java
@@ -56,24 +56,7 @@ public class DecisivenessThreshold extends Metric
 			final Trial trial = trials[trialIndex];
 			final RandomProviderState rngState = randomProviderStates[trialIndex];
 			
-			// Setup a new instance of the game
-			final Context context = Utils.setupNewContext(game, rngState);
-			
-			// Count number of times the expected winner changed.
-			double decisivenessThreshold = -1.0;
-			
-			final ArrayList<Integer> highestRankedPlayers = Utils.highestRankedPlayers(trial, context);
-			
-			for (int i = trial.numInitialPlacementMoves(); i < trial.numMoves(); i++)
-			{
-				final ArrayList<Double> allPlayerStateEvaluations = Utils.UCTAllPlayerStateEvaulations(context);
-				for (int j = 1; j < allPlayerStateEvaluations.size(); j++)
-					if (allPlayerStateEvaluations.get(j) > decisivenessThreshold && !highestRankedPlayers.contains(Integer.valueOf(j)))
-						decisivenessThreshold = allPlayerStateEvaluations.get(j);
-
-				context.game().apply(context, trial.getMove(i));
-			}
-			
+			final double decisivenessThreshold = decisivenessThreshold(game, trial, rngState);
 			avgDecisivenessThreshold += decisivenessThreshold;
 		}
 
@@ -81,5 +64,28 @@ public class DecisivenessThreshold extends Metric
 	}
 
 	//-------------------------------------------------------------------------
+	
+	public static double decisivenessThreshold(final Game game, final Trial trial, final RandomProviderState rngState)
+	{
+		// Setup a new instance of the game
+		final Context context = Utils.setupNewContext(game, rngState);
+		
+		// Count number of times the expected winner changed.
+		double decisivenessThreshold = -1.0;
+		
+		final ArrayList<Integer> highestRankedPlayers = Utils.highestRankedPlayers(trial, context);
+		
+		for (int i = trial.numInitialPlacementMoves(); i < trial.numMoves(); i++)
+		{
+			final ArrayList<Double> allPlayerStateEvaluations = Utils.UCTAllPlayerStateEvaulations(context);
+			for (int j = 1; j < allPlayerStateEvaluations.size(); j++)
+				if (allPlayerStateEvaluations.get(j) > decisivenessThreshold && !highestRankedPlayers.contains(Integer.valueOf(j)))
+					decisivenessThreshold = allPlayerStateEvaluations.get(j);
+
+			context.game().apply(context, trial.getMove(i));
+		}
+		
+		return decisivenessThreshold;
+	}
 
 }
