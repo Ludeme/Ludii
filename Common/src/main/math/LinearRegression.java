59c13bb253e9857da61ac63d54fd17b3961b59fa
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2021-07-02 12:49:50 +0200

    less warnings

--- Common/src/main/math/LinearRegression.java
+++ Common/src/main/math/LinearRegression.java
@@ -33,10 +33,10 @@ public class LinearRegression {
         int n = x.length;
 
         // first pass
-        double sumx = 0.0, sumy = 0.0, sumx2 = 0.0;
+        double sumx = 0.0, sumy = 0.0;// sumx2 = 0.0;
         for (int i = 0; i < n; i++) {
             sumx  += x[i];
-            sumx2 += x[i]*x[i];
+            //sumx2 += x[i]*x[i];
             sumy  += y[i];
         }
         double xbar = sumx / n;
