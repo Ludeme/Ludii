43728ab7e622f9d297108e62067276908a20d269
Matthew Stephenson <matthew.stephenson@maastrichtuniversity.nl>
2021-09-01 18:35:54 +0200

    few bug fixes

--- PlayerDesktop/src/app/tutorialVisualisation/MoveVisualisation.java
+++ PlayerDesktop/src/app/tutorialVisualisation/MoveVisualisation.java
@@ -13,6 +13,7 @@ import app.display.screenCapture.ScreenCapture;
 import app.utils.GameUtil;
 import manager.Referee;
 import other.action.Action;
+import other.action.ActionType;
 import other.context.Context;
 import other.location.FullLocation;
 import other.location.Location;
@@ -27,6 +28,9 @@ public class MoveVisualisation
 	
 	/** How many trials to run to provide all the moves for analysis. */
 	private final static int numberTrials = 1;
+	
+	/** If gif animations should be saved rather than static screenshots. */
+	private final static boolean gifAnimations = true;
 	
 	//-------------------------------------------------------------------------
 	
@@ -69,19 +73,15 @@ public class MoveVisualisation
 	 */
 	public static void moveVisualisation(final PlayerApp app)
 	{
-		// Turn on some settings
 		app.settingsPlayer().setPerformingTutorialVisualisation(true);
-		
 		DesktopApp.frame().setSize(300, 465);
 		app.repaint();
 		
 		final Referee ref = app.manager().ref();
-		
+
+		// Generate all trials that will be used.
 		final List<Trial> generatedTrials = new ArrayList<>();
 		final List<RandomProviderDefaultState> generatedTrialsRNG = new ArrayList<>();
-		
-		final List<MoveCompleteInformation> condensedMoveList = new ArrayList<>();
-		
 		while (generatedTrials.size() < numberTrials)
 		{
 			app.restartGame();
@@ -90,8 +90,11 @@ public class MoveVisualisation
 			generatedTrialsRNG.add(new RandomProviderDefaultState(app.manager().currGameStartRngState().getState()));
 		}
 		
+		// Merge all similar moves from our generated trials into a condensed moves list.
+		final List<MoveCompleteInformation> condensedMoveList = new ArrayList<>();
 		for (int trialIndex = 0; trialIndex < generatedTrials.size(); trialIndex++)
 		{
+			// Reset the game for the new trial.
 			final Trial trial = generatedTrials.get(trialIndex);
 			final RandomProviderDefaultState trialRNG = generatedTrialsRNG.get(trialIndex);
 			app.manager().setCurrGameStartRngState(trialRNG);
@@ -99,11 +102,12 @@ public class MoveVisualisation
 			
 			for (int i = trial.numInitialPlacementMoves(); i < trial.numMoves(); i++)
 			{
+				// Get complete information about the selected move
 				final Move move = trial.getMove(i);
-				
 				final int what = getWhatOfMove(ref.context(), move);
 				final MoveCompleteInformation newMove = new MoveCompleteInformation(trial, trialRNG, i, what);
 				
+				// Determine if the move should be added to the condensed list.
 				boolean addMove = true;
 				for (final MoveCompleteInformation priorMove : condensedMoveList)
 				{
@@ -113,39 +117,39 @@ public class MoveVisualisation
 						break;
 					}
 				}
-				
 				if (addMove)
 					condensedMoveList.add(newMove);
 				
+				// Apply the move to update the context for the next move.
 				ref.context().game().apply(ref.context(), move);
 			}
 		}
 		
+		System.out.println("Total of " + condensedMoveList.size() + " condensed moves found.");
+		
+		// TODO
+		// Check the trials for the state where the most of a specific condensed move are legal.
+		
+		// Take a screenshot/video of every move in the condensed list.
 		final Timer screenshotTimer = new Timer();
 		screenshotTimer.scheduleAtFixedRate(new TimerTask()
 		{
-			int index = 0;
+			int condensedMoveIndex = 0;
 			
 		    @Override
 		    public void run()
 		    {
-		    	if (index >= condensedMoveList.size())
+		    	if (condensedMoveIndex >= condensedMoveList.size())
 		    	{
-		    		System.out.println("Screenshots complete.");
+		    		System.out.println("Process complete.");
 		    		screenshotTimer.cancel();
 		    		screenshotTimer.purge();
 		    	}
 		    	else
 		    	{
-			    	final MoveCompleteInformation moveInformation = condensedMoveList.get(index);
-			    	
-			    	System.out.println("-----------------");
-					System.out.println(moveInformation);
-					
-			    	//takeMoveScreenshot(app, moveInformation);
-					takeMoveImage(app, moveInformation, true);
-			    	
-			    	index++;
+			    	final MoveCompleteInformation moveInformation = condensedMoveList.get(condensedMoveIndex);
+					takeMoveImage(app, moveInformation);
+					condensedMoveIndex++;
 		    	}
 		    }
 		}, 0, 5000);
@@ -156,26 +160,28 @@ public class MoveVisualisation
 	/** 
 	 * Takes a screenshot/gif of the app directly after the move is made. 
 	 */
-	protected final static void takeMoveImage(final PlayerApp app, final MoveCompleteInformation moveInformation, final boolean gif)
+	protected final static void takeMoveImage(final PlayerApp app, final MoveCompleteInformation moveInformation)
 	{
 		final Referee ref = app.manager().ref();
 		
+		// Reset the game for the new trial.
 		final Trial trial = moveInformation.trial;
 		final RandomProviderDefaultState trialRNG = moveInformation.rng;
 		app.manager().setCurrGameStartRngState(trialRNG);
 		GameUtil.resetGame(app, true);
 		
+		// Apply all moves up until the one we want to capture.
 		for (int i = trial.numInitialPlacementMoves(); i < moveInformation.moveIndex; i++)
 		{
 			final Move move = trial.getMove(i);
 			ref.context().game().apply(ref.context(), move);
 		}
 		
+		// Update the GUI
 		app.contextSnapshot().setContext(ref.context());
 		app.repaint();
 		
-		//  && MoveAnimation.getMoveAnimationType(app, trial.getMove(moveInformation.moveIndex)) != AnimationType.NONE
-		if (gif)
+		if (gifAnimations)	// Record a short gif animation of the move.				&& MoveAnimation.getMoveAnimationType(app, trial.getMove(moveInformation.moveIndex)) != AnimationType.NONE
 		{
 			new java.util.Timer().schedule
 			( 
@@ -191,10 +197,8 @@ public class MoveVisualisation
 		        1000 
 			);	
 		}
-		else
+		else	// Take a screenshot before and after.
 		{
-			app.settingsPlayer().setShowLastMove(true);
-			ref.applyHumanMoveToGame(app.manager(), trial.getMove(moveInformation.moveIndex));
 			new java.util.Timer().schedule
 			( 
 		        new java.util.TimerTask() 
@@ -202,10 +206,36 @@ public class MoveVisualisation
 		            @Override
 		            public void run() 
 		            {
-		            	ScreenCapture.gameScreenshot("tutorialVisualisation/" + moveInformation.toString());
+		            	ScreenCapture.gameScreenshot("tutorialVisualisation/screenshot/BEFORE_" + moveInformation.toString());
 		            }
 		        }, 
 		        1000 
+			);
+
+			new java.util.Timer().schedule
+			( 
+		        new java.util.TimerTask() 
+		        {
+		            @Override
+		            public void run() 
+		            {
+		            	ref.applyHumanMoveToGame(app.manager(), trial.getMove(moveInformation.moveIndex));
+		            }
+		        }, 
+		        2000 
+			);
+			
+			new java.util.Timer().schedule
+			( 
+		        new java.util.TimerTask() 
+		        {
+		            @Override
+		            public void run() 
+		            {
+		            	ScreenCapture.gameScreenshot("tutorialVisualisation/screenshot/AFTER_" + moveInformation.toString());
+		            }
+		        }, 
+		        3000 
 			);
 		}
 	}
@@ -227,8 +257,14 @@ public class MoveVisualisation
 			return false;
 		
 		for (int i = 0; i < m1.move().actions().size(); i++)
-			if (!m1.move().actions().get(i).actionType().equals(m2.move().actions().get(i).actionType()))
+		{
+			final ActionType m1ActionType = m1.move().actions().get(i).actionType();
+			final ActionType m2ActionType = m2.move().actions().get(i).actionType();
+			if (m1ActionType != null && m2ActionType != null && !m1ActionType.equals(m2ActionType))
 				return false;
+			else if (m1ActionType == null && m2ActionType != null || m1ActionType != null && m2ActionType == null)
+				return false;
+		}
 		
 		// m.direction(ref.context()
 		
