8a222a6829140da38aafedeb956c2ecc2cbe4ba3
DennisSoemers <d.soemers@gmail.com>
2021-09-07 16:38:48 +0200

    scripts fix

--- Player/src/supplementary/experiments/scripts/EvalTrainedFeaturesFinalStatesBuffersScriptsGen.java
+++ Player/src/supplementary/experiments/scripts/EvalTrainedFeaturesFinalStatesBuffersScriptsGen.java
@@ -153,8 +153,8 @@ public class EvalTrainedFeaturesFinalStatesBuffersScriptsGen
 								(
 									",", 
 									"playout=softmax",
-									"policyweights1=/home/" + userName + "TrainFeaturesFinalStatesBuffers/Out/" + processData.gameName + "_Without/PolicyWeightsCE_P1_00201.txt",
-									"policyweights2=/home/" + userName + "TrainFeaturesFinalStatesBuffers/Out/" + processData.gameName + "_Without/PolicyWeightsCE_P2_00201.txt"
+									"policyweights1=/home/" + userName + "TrainFeaturesFinalStatesBuffers/Out/" + StringRoutines.cleanGameName(processData.gameName.replaceAll(Pattern.quote(".lud"), "")) + "_Without/PolicyWeightsCE_P1_00201.txt",
+									"policyweights2=/home/" + userName + "TrainFeaturesFinalStatesBuffers/Out/" + StringRoutines.cleanGameName(processData.gameName.replaceAll(Pattern.quote(".lud"), "")) + "_Without/PolicyWeightsCE_P2_00201.txt"
 								),
 								"tree_reuse=true",
 								"num_threads=2",
