bc2eab9532609c16c2133e25cff3260fd4b3eb01
matthew <matthew@matthew-ThinkPad-X1-Extreme-Gen-3>
2021-09-14 16:18:46 +0200

    change function

--- Mining/src/agentPrediction/external/AgentPredictionExternal.java
+++ Mining/src/agentPrediction/external/AgentPredictionExternal.java
@@ -166,7 +166,8 @@ public class AgentPredictionExternal
 	 */
 	private static String conceptNameString(final boolean compilationOnly)
 	{
-		final Concept[] concepts = Concept.values();
+		// final Concept[] concepts = Concept.values();
+		final Concept[] concepts = Concept.portfolioConcepts();
 		final StringBuffer sb = new StringBuffer();
 		for (final Concept concept: concepts)
 			if (!compilationOnly || concept.computationType().equals(ConceptComputationType.Compilation))
@@ -184,7 +185,8 @@ public class AgentPredictionExternal
 	 */
 	private static String conceptValueString(final Game game, final boolean compilationOnly)
 	{
-		final Concept[] concepts = Concept.values();
+		// final Concept[] concepts = Concept.values();
+		final Concept[] concepts = Concept.portfolioConcepts();
 		final StringBuffer sb = new StringBuffer();
 		for (final Concept concept: concepts)
 			if (!compilationOnly || concept.computationType().equals(ConceptComputationType.Compilation))
