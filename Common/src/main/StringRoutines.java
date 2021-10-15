957d0c21090262b8bc5040bc98190e88506976bf
matthew <matthew@matthew-ThinkPad-X1-Extreme-Gen-3>
2021-10-15 10:51:07 +0200

    tidying code

--- Common/src/main/StringRoutines.java
+++ Common/src/main/StringRoutines.java
@@ -795,39 +795,6 @@ public final class StringRoutines
 	}
 
 	//-------------------------------------------------------------------------
-
-	
-	/**
-	 * From: https://rosettacode.org/wiki/Levenshtein_distance#Java
-	 * @return Levenshtein (i.e. edit) distance between two strings.
-	 */
-	public static int levenshteinDistance(final String strA, final String strB) 
-	{
-        final String a = strA.toLowerCase();
-        final String b = strB.toLowerCase();
-        
-        // i == 0
-        final int [] costs = new int [b.length() + 1];
-        
-        for (int j = 0; j < costs.length; j++)
-            costs[j] = j;
-        
-        for (int i = 1; i <= a.length(); i++) 
-        {
-            // j == 0; nw = lev(i - 1, j)
-            costs[0] = i;
-            int nw = i - 1;
-            for (int j = 1; j <= b.length(); j++) 
-            {
-                final int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]), a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
-                nw = costs[j];
-                costs[j] = cj;
-            }
-        }
-        return costs[b.length()];
-	}
-
-	//-------------------------------------------------------------------------
 	
 	public static String getPlural(String string) 
 	{
