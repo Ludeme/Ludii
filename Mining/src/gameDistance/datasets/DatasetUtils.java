6be872eed1de4cdd66031eded418c089ac7f021b
matthew <matthew@matthew-ThinkPad-X1-Extreme-Gen-3>
2021-10-20 15:32:15 +0200

    dataset utils updated to use new saved trials

--- Mining/src/gameDistance/datasets/DatasetUtils.java
+++ Mining/src/gameDistance/datasets/DatasetUtils.java
@@ -1,14 +1,12 @@
 package gameDistance.datasets;
 
-import static org.junit.Assert.fail;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import game.Game;
 import manager.utils.game_logs.MatchRecord;
@@ -32,43 +30,81 @@ public class DatasetUtils
 	 * @param candidateGameName
 	 * @param rulesetStrings
 	 */
-	private static void loadSavedTrials(final String candidateGameName, final List<String> rulesetStrings)
+	private static void loadSavedTrials(final Game game, final String keyName)
 	{
-		final String ludPath = candidateGameName.replaceAll(Pattern.quote("\\"), "/");
-		final String trialDirPath = "random_trials" + ludPath.replace(".lud", "");
+		final String folderTrials = "/../Trials/TrialsRandom/";
+		final File currentFolder = new File(".");
 
-		final File trialsDir = new File(trialDirPath);
+		final File folder = new File(currentFolder.getAbsolutePath() + folderTrials);
+		final String gameName = game.name();
+		final String rulesetName = game.getRuleset() == null ? "" : game.getRuleset().heading();
 
-		if (!trialsDir.exists())
-			System.err.println("WARNING: No directory of trials exists at: " + trialsDir.getAbsolutePath());
+		String trialFolderPath = folder + "/" + gameName;
+		if(!rulesetName.isEmpty())
+			trialFolderPath += File.separator + rulesetName.replace("/", "_");
 
-		final File[] trialFiles = trialsDir.listFiles();
+		final File trialFolder = new File(trialFolderPath);
+		
+		if(!trialFolder.exists())
+			System.out.println("DO NOT FOUND IT - Path is " + trialFolder);
 
-		if (trialFiles.length == 0)
-			System.err.println("WARNING: No trial files exist in directory: " + trialsDir.getAbsolutePath());
-
-		// Parse and compile the game
-		final Game game = GameLoader.loadGameFromName(candidateGameName, rulesetStrings);
-		if (game == null)
-			fail("COMPILATION FAILED for the file : " + ludPath);
-
-		final String keyName = candidateGameName + rulesetStrings.toString();
 		gameTrials.put(keyName, new ArrayList<>());
 		
-		for (final File trialFile : trialFiles)
+		for(final File trialFile : trialFolder.listFiles())
 		{
-			System.out.println("Testing re-play of trial: " + trialFile);
+			MatchRecord loadedRecord;
 			try
 			{
-				final MatchRecord loadedRecord = MatchRecord.loadMatchRecordFromTextFile(trialFile, game);
+				loadedRecord = MatchRecord.loadMatchRecordFromTextFile(trialFile, game);
 				final Trial loadedTrial = loadedRecord.trial();
 				gameTrials.get(keyName).add(new Trial(loadedTrial));
+			}
+			catch (final FileNotFoundException e)
+			{
+				e.printStackTrace();
 			}
 			catch (final IOException e)
 			{
 				e.printStackTrace();
 			}
 		}
+		
+
+//		final String ludPath = candidateGameName.replaceAll(Pattern.quote("\\"), "/");
+//		final String trialDirPath = "random_trials" + ludPath.replace(".lud", "");
+//
+//		final File trialsDir = new File(trialDirPath);
+//
+//		if (!trialsDir.exists())
+//			System.err.println("WARNING: No directory of trials exists at: " + trialsDir.getAbsolutePath());
+//
+//		final File[] trialFiles = trialsDir.listFiles();
+//
+//		if (trialFiles.length == 0)
+//			System.err.println("WARNING: No trial files exist in directory: " + trialsDir.getAbsolutePath());
+//
+//		// Parse and compile the game
+//		final Game game = GameLoader.loadGameFromName(candidateGameName, rulesetStrings);
+//		if (game == null)
+//			fail("COMPILATION FAILED for the file : " + ludPath);
+//
+//		final String keyName = candidateGameName + rulesetStrings.toString();
+//		gameTrials.put(keyName, new ArrayList<>());
+//		
+//		for (final File trialFile : trialFiles)
+//		{
+//			System.out.println("Testing re-play of trial: " + trialFile);
+//			try
+//			{
+//				final MatchRecord loadedRecord = MatchRecord.loadMatchRecordFromTextFile(trialFile, game);
+//				final Trial loadedTrial = loadedRecord.trial();
+//				gameTrials.get(keyName).add(new Trial(loadedTrial));
+//			}
+//			catch (final IOException e)
+//			{
+//				e.printStackTrace();
+//			}
+//		}
 	}
 	
 	//-------------------------------------------------------------------------
@@ -77,11 +113,12 @@ public class DatasetUtils
 	{
 		final String gamePath = GameLoader.getFilePath(game.name());
 		final List<String> rulesetOptions = game.getOptions();
+		final String keyName = gamePath + rulesetOptions.toString();
 		
-		if (!gameTrials.containsKey(gamePath + rulesetOptions.toString()))
-			loadSavedTrials(gamePath, rulesetOptions);
+		if (!gameTrials.containsKey(keyName))
+			loadSavedTrials(game, keyName);
 		
-		return gameTrials.get(gamePath + rulesetOptions.toString());
+		return gameTrials.get(keyName);
 	}
 	
 	//-------------------------------------------------------------------------
