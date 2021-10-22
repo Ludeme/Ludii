60f95ffe78edffd53932d4c209effccaa4f4a4a7
matthew <matthew@matthew-ThinkPad-X1-Extreme-Gen-3>
2021-10-22 09:54:46 +0200

    compare distance metrics updated to store output in csv

--- Mining/src/gameDistance/CompareAllDistanceMetrics.java
+++ Mining/src/gameDistance/CompareAllDistanceMetrics.java
@@ -1,5 +1,11 @@
 package gameDistance;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import game.Game;
@@ -17,6 +23,7 @@ import gameDistance.metrics.sequence.Levenshtein;
 import gameDistance.metrics.sequence.LocalAlignment;
 import gameDistance.metrics.sequence.RepeatedLocalAlignment;
 import gameDistance.metrics.treeEdit.ZhangShasha;
+import main.FileHandling;
 import other.GameLoader;
 
 /**
@@ -29,13 +36,15 @@ public class CompareAllDistanceMetrics
 	
 	//---------------------------------------------------------------------
 
+	final static String outputPath = "res/gameDistance/";
+	
 	final static Dataset ludemeDataset = new LudemeDataset();
 	final static Dataset booleanConceptDataset = new BooleanConceptDataset();
 	final static Dataset moveConceptDataset = new MoveConceptDataset();
 	
-	static Map<String, Double> ludemeVocabulary;
-	static Map<String, Double> booleanConceptVocabulary;
-	static Map<String, Double> moveConceptVocabulary;
+	static Map<String, Double> fullLudemeVocabulary;
+	static Map<String, Double> fullBooleanConceptVocabulary;
+	static Map<String, Double> fullMoveConceptVocabulary;
 	
 	final static int nGramLength = 4;
 	
@@ -43,32 +52,98 @@ public class CompareAllDistanceMetrics
 	
 	public static void main(final String[] args)
 	{
-		ludemeVocabulary = DistanceUtils.fullVocabulary(ludemeDataset);
+		fullLudemeVocabulary = DistanceUtils.fullVocabulary(ludemeDataset);
 		System.out.println("ludemeVocabulary stored");
 		
-		booleanConceptVocabulary = DistanceUtils.fullVocabulary(booleanConceptDataset);
+		fullBooleanConceptVocabulary = DistanceUtils.fullVocabulary(booleanConceptDataset);
 		System.out.println("booleanConceptVocabulary stored");
 		
-		moveConceptVocabulary = DistanceUtils.fullVocabulary(moveConceptDataset);
+		fullMoveConceptVocabulary = DistanceUtils.fullVocabulary(moveConceptDataset);
 		System.out.println("moveConceptVocabulary stored");
 		
-		final String[] gamesToCompare = 
-			{
-				"/lud/board/hunt/Adugo.lud",
-				"/lud/board/war/replacement/checkmate/chess/Half Chess.lud",
-				"/lud/board/war/replacement/checkmate/chess/Chess.lud"
-			};
+//		final String[] gamesToCompare = 
+//			{
+//				"/lud/board/hunt/Adugo.lud",
+//				"/lud/board/war/replacement/checkmate/chess/Half Chess.lud",
+//				"/lud/board/war/replacement/checkmate/chess/Chess.lud"
+//			};
 		
+		final String[] allGames = FileHandling.listGames();
+		final List<String> allGamesToCompare = new ArrayList<>();
+		for (final String gameName : allGames)
+			if (!FileHandling.shouldIgnoreLudAnalysis(gameName))
+				allGamesToCompare.add(gameName);
+		final String[] gamesToCompare = allGamesToCompare.toArray(new String[0]);
+		
+
 		for (int i = 0; i < gamesToCompare.length; i++)
-			for (int j = 0; j < gamesToCompare.length; j++)
-				if (i < j)
-					compareTwoGames(GameLoader.loadGameFromName(gamesToCompare[i]), GameLoader.loadGameFromName(gamesToCompare[j]));	
+		{
+			System.out.println(gamesToCompare[i]);
+			final File outputFile = new File(outputPath + gamesToCompare[i].replaceAll("[^a-zA-Z0-9]", "_") + ".csv");
+			try
+			{
+				outputFile.createNewFile();
+			}
+			catch (final IOException e1)
+			{
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} 
+
+			try (FileWriter myWriter = new FileWriter(outputFile))
+			{
+				final Map<String, Map<String, Double>> allGameDistances = new HashMap<>();
+				
+				for (int j = 0; j < gamesToCompare.length; j++)
+					allGameDistances.put(gamesToCompare[j], compareTwoGames(GameLoader.loadGameFromName(gamesToCompare[i]), GameLoader.loadGameFromName(gamesToCompare[j])));
+				
+				// Write the top row of the csv
+				String topRow = "GameName";
+				@SuppressWarnings("unchecked")
+				final List<String> distanceNames = new ArrayList<>(((Map<String, Double>) allGameDistances.values().toArray()[0]).keySet());
+				for (final String distance : distanceNames)
+				{
+					topRow += "," + distance;
+				}
+				myWriter.write(topRow + "\n");
+				
+				for (final Map.Entry<String, Map<String, Double>> gameEntry : allGameDistances.entrySet()) 
+				{
+				    final String gameName = gameEntry.getKey();
+				    final List<String> row =  new ArrayList<>(distanceNames);
+				    
+				    for (final Map.Entry<String, Double> distanceEntry : gameEntry.getValue().entrySet()) 
+					{
+				    	final String distanceMetric = distanceEntry.getKey();
+				    	final Double distaneValue = distanceEntry.getValue();
+				    	
+				    	row.set(row.indexOf(distanceMetric), String.valueOf(distaneValue));
+					}
+				    
+				    myWriter.write(gameName + "," + String.join(",", row) + "\n");
+				}
+				
+				myWriter.close();
+			}
+			catch (final IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		
+//		for (int i = 0; i < gamesToCompare.length; i++)
+//			for (int j = 0; j < gamesToCompare.length; j++)
+//				if (i < j)
+//					compareTwoGames(GameLoader.loadGameFromName(gamesToCompare[i]), GameLoader.loadGameFromName(gamesToCompare[j]));
 	}
 	
 	//---------------------------------------------------------------------
 	
-	public static void compareTwoGames(Game gameA, Game gameB)
+	public static Map<String, Double> compareTwoGames(final Game gameA, final Game gameB)
 	{
+		final Map<String, Double> allDistances = new HashMap<>();
+		
 		ludemeDataset.getTree(gameA);
 		System.out.println("\n" + gameA.name() + " v.s. " + gameB.name());
 		
@@ -87,96 +162,98 @@ public class CompareAllDistanceMetrics
 		// JensenShannonDivergence
 		
 		final DistanceMetric jensenShannonDivergenceMetric = new JensenShannonDivergence();
-		System.out.println("JensenShannonDivergence");
+		//System.out.println("JensenShannonDivergence");
 		
-		System.out.println(jensenShannonDivergenceMetric.distance(ludemeDataset, defaultLudemeVocabulary, gameA, gameB));
-		System.out.println(jensenShannonDivergenceMetric.distance(booleanConceptDataset, defaultBooleanConceptVocabulary, gameA, gameB));
-		System.out.println(jensenShannonDivergenceMetric.distance(moveConceptDataset, defaultMoveConceptVocabulary, gameA, gameB));
+		allDistances.put("JSD_ludeme", jensenShannonDivergenceMetric.distance(ludemeDataset, defaultLudemeVocabulary, gameA, gameB));
+		allDistances.put("JSD_booleanConcept", jensenShannonDivergenceMetric.distance(booleanConceptDataset, defaultBooleanConceptVocabulary, gameA, gameB));
+		allDistances.put("JSD_moveConcept", jensenShannonDivergenceMetric.distance(moveConceptDataset, defaultMoveConceptVocabulary, gameA, gameB));
 		
-		System.out.println(jensenShannonDivergenceMetric.distance(new NGramDataset(ludemeDataset, nGramLength), defaultLudemeNGramVocabulary, gameA, gameB));
-		System.out.println(jensenShannonDivergenceMetric.distance(new NGramDataset(moveConceptDataset, nGramLength), defaultMoveConceptNGramVocabulary, gameA, gameB));
+		allDistances.put("JSD_ludeme_ngram", jensenShannonDivergenceMetric.distance(new NGramDataset(ludemeDataset, nGramLength), defaultLudemeNGramVocabulary, gameA, gameB));
+		allDistances.put("JSD_moveCooncept_ngram", jensenShannonDivergenceMetric.distance(new NGramDataset(moveConceptDataset, nGramLength), defaultMoveConceptNGramVocabulary, gameA, gameB));
 		
-		System.out.println(jensenShannonDivergenceMetric.distance(ludemeDataset, ludemeVocabulary, gameA, gameB));
-		System.out.println(jensenShannonDivergenceMetric.distance(booleanConceptDataset, booleanConceptVocabulary, gameA, gameB));
-		System.out.println(jensenShannonDivergenceMetric.distance(moveConceptDataset, moveConceptVocabulary, gameA, gameB));
+		allDistances.put("JSD_ludeme_TFIDF", jensenShannonDivergenceMetric.distance(ludemeDataset, fullLudemeVocabulary, gameA, gameB));
+		allDistances.put("JSD_booleanConcept_TFIDF", jensenShannonDivergenceMetric.distance(booleanConceptDataset, fullBooleanConceptVocabulary, gameA, gameB));
+		allDistances.put("JSD_moveConcept_TFIDF", jensenShannonDivergenceMetric.distance(moveConceptDataset, fullMoveConceptVocabulary, gameA, gameB));
 		
 		//---------------------------------------------------------------------
 		// Cosine
 		
 		final DistanceMetric cosineMetric = new Cosine();
-		System.out.println("Cosine");
+		//System.out.println("Cosine");
 		
-		System.out.println(cosineMetric.distance(ludemeDataset, defaultLudemeVocabulary, gameA, gameB));
-		System.out.println(cosineMetric.distance(booleanConceptDataset, defaultBooleanConceptVocabulary, gameA, gameB));
-		System.out.println(cosineMetric.distance(moveConceptDataset, defaultMoveConceptVocabulary, gameA, gameB));
+		allDistances.put("Cosine_ludeme", cosineMetric.distance(ludemeDataset, defaultLudemeVocabulary, gameA, gameB));
+		allDistances.put("Cosine_booleanConcept", cosineMetric.distance(booleanConceptDataset, defaultBooleanConceptVocabulary, gameA, gameB));
+		allDistances.put("Cosine_moveConcept", cosineMetric.distance(moveConceptDataset, defaultMoveConceptVocabulary, gameA, gameB));
 		
-		System.out.println(cosineMetric.distance(new NGramDataset(ludemeDataset, nGramLength), defaultLudemeNGramVocabulary, gameA, gameB));
-		System.out.println(cosineMetric.distance(new NGramDataset(moveConceptDataset, nGramLength), defaultMoveConceptNGramVocabulary, gameA, gameB));
+		allDistances.put("Cosine_ludeme_ngram", cosineMetric.distance(new NGramDataset(ludemeDataset, nGramLength), defaultLudemeNGramVocabulary, gameA, gameB));
+		allDistances.put("Cosine_moveCooncept_ngram", cosineMetric.distance(new NGramDataset(moveConceptDataset, nGramLength), defaultMoveConceptNGramVocabulary, gameA, gameB));
 		
-		System.out.println(cosineMetric.distance(ludemeDataset, ludemeVocabulary, gameA, gameB));
-		System.out.println(cosineMetric.distance(booleanConceptDataset, booleanConceptVocabulary, gameA, gameB));
-		System.out.println(cosineMetric.distance(moveConceptDataset, moveConceptVocabulary, gameA, gameB));
+		allDistances.put("Cosine_ludeme_TFIDF", cosineMetric.distance(ludemeDataset, fullLudemeVocabulary, gameA, gameB));
+		allDistances.put("Cosine_booleanConcept_TFIDF", cosineMetric.distance(booleanConceptDataset, fullBooleanConceptVocabulary, gameA, gameB));
+		allDistances.put("Cosine_moveConcept_TFIDF", cosineMetric.distance(moveConceptDataset, fullMoveConceptVocabulary, gameA, gameB));
 		
 		//---------------------------------------------------------------------
 		// Jaccard
 		
 		final DistanceMetric jaccardMetric = new Jaccard();
-		System.out.println("Jaccard");
+		//System.out.println("Jaccard");
 		
-		System.out.println(jaccardMetric.distance(ludemeDataset, defaultLudemeVocabulary, gameA, gameB));
-		System.out.println(jaccardMetric.distance(booleanConceptDataset, defaultBooleanConceptVocabulary, gameA, gameB));
-		System.out.println(jaccardMetric.distance(moveConceptDataset, defaultMoveConceptVocabulary, gameA, gameB));
+		allDistances.put("Jaccard_ludeme", jaccardMetric.distance(ludemeDataset, defaultLudemeVocabulary, gameA, gameB));
+		allDistances.put("Jaccard_booleanConcept", jaccardMetric.distance(booleanConceptDataset, defaultBooleanConceptVocabulary, gameA, gameB));
+		allDistances.put("Jaccard_moveConcept", jaccardMetric.distance(moveConceptDataset, defaultMoveConceptVocabulary, gameA, gameB));
 		
-		System.out.println(jaccardMetric.distance(new NGramDataset(ludemeDataset, nGramLength), defaultLudemeNGramVocabulary, gameA, gameB));
-		System.out.println(jaccardMetric.distance(new NGramDataset(moveConceptDataset, nGramLength), defaultMoveConceptNGramVocabulary, gameA, gameB));
+		allDistances.put("Jaccard_ludeme_ngram", jaccardMetric.distance(new NGramDataset(ludemeDataset, nGramLength), defaultLudemeNGramVocabulary, gameA, gameB));
+		allDistances.put("Jaccard_moveCooncept_ngram", jaccardMetric.distance(new NGramDataset(moveConceptDataset, nGramLength), defaultMoveConceptNGramVocabulary, gameA, gameB));
 		
-		System.out.println(jaccardMetric.distance(ludemeDataset, ludemeVocabulary, gameA, gameB));
-		System.out.println(jaccardMetric.distance(booleanConceptDataset, booleanConceptVocabulary, gameA, gameB));
-		System.out.println(jaccardMetric.distance(moveConceptDataset, moveConceptVocabulary, gameA, gameB));
+		allDistances.put("Jaccard_ludeme_TFIDF", jaccardMetric.distance(ludemeDataset, fullLudemeVocabulary, gameA, gameB));
+		allDistances.put("Jaccard_booleanConcept_TFIDF", jaccardMetric.distance(booleanConceptDataset, fullBooleanConceptVocabulary, gameA, gameB));
+		allDistances.put("Jaccard_moveConcept_TFIDF", jaccardMetric.distance(moveConceptDataset, fullMoveConceptVocabulary, gameA, gameB));
 		
 		//---------------------------------------------------------------------
 		// Levenshtein
 		
 		final DistanceMetric levenshteinMetric = new Levenshtein();
-		System.out.println("Levenshtein");
+		//System.out.println("Levenshtein");
 		
-		System.out.println(levenshteinMetric.distance(ludemeDataset, null, gameA, gameB));
-		System.out.println(levenshteinMetric.distance(moveConceptDataset, null, gameA, gameB));
+		allDistances.put("Levenshtein_ludeme", levenshteinMetric.distance(ludemeDataset, null, gameA, gameB));
+		allDistances.put("Levenshtein_moveConcept", levenshteinMetric.distance(moveConceptDataset, null, gameA, gameB));
 		
 		//---------------------------------------------------------------------
 		// Local Alignment
 		
 		final DistanceMetric localAlignmentMetric = new LocalAlignment();
-		System.out.println("LocalAlignment");
+		//System.out.println("LocalAlignment");
 		
-		System.out.println(localAlignmentMetric.distance(ludemeDataset, null, gameA, gameB));
-		System.out.println(localAlignmentMetric.distance(moveConceptDataset, null, gameA, gameB));
+		allDistances.put("LocalAlignment_ludeme", localAlignmentMetric.distance(ludemeDataset, null, gameA, gameB));
+		allDistances.put("LocalAlignment_moveConcept", localAlignmentMetric.distance(moveConceptDataset, null, gameA, gameB));
 		
 		//---------------------------------------------------------------------
 		// Repeated Local Alignment
 		
 		final DistanceMetric repeatedLocalAlignmentMetric = new RepeatedLocalAlignment();
-		System.out.println("RepeatedLocalAlignment");
+		//System.out.println("RepeatedLocalAlignment");
 		
-		System.out.println(repeatedLocalAlignmentMetric.distance(ludemeDataset, null, gameA, gameB));
-		System.out.println(repeatedLocalAlignmentMetric.distance(moveConceptDataset, null, gameA, gameB));
+		allDistances.put("RepeatedLocalAlignment_ludeme", repeatedLocalAlignmentMetric.distance(ludemeDataset, null, gameA, gameB));
+		allDistances.put("RepeatedLocalAlignment_moveConcept", repeatedLocalAlignmentMetric.distance(moveConceptDataset, null, gameA, gameB));
 		
 		//---------------------------------------------------------------------
 		// Global Alignment
 		
 		final DistanceMetric globalAlignmentMetric = new GlobalAlignment();
-		System.out.println("GlobalAlignment");
+		//System.out.println("GlobalAlignment");
 		
-		System.out.println(globalAlignmentMetric.distance(ludemeDataset, null, gameA, gameB));
-		System.out.println(globalAlignmentMetric.distance(moveConceptDataset, null, gameA, gameB));
+		allDistances.put("GlobalAlignment_ludeme", globalAlignmentMetric.distance(ludemeDataset, null, gameA, gameB));
+		allDistances.put("GlobalAlignment_moveConcept", globalAlignmentMetric.distance(moveConceptDataset, null, gameA, gameB));
 		
 		//---------------------------------------------------------------------
 		// Zhang Shasha
 		
 		final DistanceMetric zhangShashaMetric = new ZhangShasha();
-		System.out.println("ZhangShasha");
+		//System.out.println("ZhangShasha");
 		
-		System.out.println(zhangShashaMetric.distance(ludemeDataset, defaultLudemeVocabulary, gameA, gameB));
+		allDistances.put("ZhangShasha_ludeme", zhangShashaMetric.distance(ludemeDataset, defaultLudemeVocabulary, gameA, gameB));
+		
+		return allDistances;
 	}
 	
 	//---------------------------------------------------------------------
