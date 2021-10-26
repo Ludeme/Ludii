639b60a4d729ec4e1cb53e7679b3ceab20735ebd
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2021-10-26 08:42:00 +0200

    stuff from distance computation

--- Mining/src/gameDistance/CompareAllDistanceMetrics.java
+++ Mining/src/gameDistance/CompareAllDistanceMetrics.java
@@ -61,17 +61,49 @@ public class CompareAllDistanceMetrics
 		System.out.println("moveConceptVocabulary stored");
 		
 		// Specific games/rulesets to compare.
+//		final String[] gamesToCompare = 
+//			{
+//				"/lud/board/race/escape/Royal Game of Ur.lud",
+//				"/lud/board/race/escape/Royal Game of Ur.lud",
+//				"/lud/board/war/replacement/checkmate/chess/Chess.lud"
+//			};
+//		final String[] rulesetsToCompare = 
+//			{
+//				"Ruleset/Finkel (Scholarly)",
+//				"Ruleset/Murray (Suggested)",
+//				""
+//			};
+		
 		final String[] gamesToCompare = 
 			{
-				"/lud/board/race/escape/Royal Game of Ur.lud",
-				"/lud/board/race/escape/Royal Game of Ur.lud",
-				"/lud/board/war/replacement/checkmate/chess/Chess.lud"
+				"/lud/board/hunt/Haretavl.lud",
+				"/lud/board/hunt/Jeu Militaire.lud",
+				"/lud/board/hunt/Jeu Militaire.lud",
+				"/lud/board/hunt/Hund efter Hare (Thy).lud",
+				"/lud/board/hunt/Hund efter Hare (Vendsyssel).lud",
+				"/lud/board/hunt/Hyvn aetter Hare.lud",
+				"/lud/board/hunt/Janes Soppi.lud",
+				"/lud/board/space/blocking/Janes Soppi (Symmetrical).lud",
+				"/lud/board/hunt/Gioco dell'Orso.lud",
+				"/lud/board/hunt/La Liebre Perseguida.lud",
+				"/lud/board/hunt/Neg Tugal Tuux.lud",
+				"/lud/board/hunt/Uxrijn Ever.lud",
 			};
+
 		final String[] rulesetsToCompare = 
 			{
-				"Ruleset/Finkel (Scholarly)",
-				"Ruleset/Murray (Suggested)",
-				""
+				"",
+				"Ruleset/Lucas (Described)",
+				"Ruleset/Gardner (Suggested)",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
+				"",
 			};
 		
 		// Compare all games/rulesets.
