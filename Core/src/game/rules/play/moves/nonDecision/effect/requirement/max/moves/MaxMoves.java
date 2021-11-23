d0393a257cebec2282c5d9275914cbd790724719
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2021-11-23 18:04:58 +0100

    for tomorrow

--- Core/src/game/rules/play/moves/nonDecision/effect/requirement/max/moves/MaxMoves.java
+++ Core/src/game/rules/play/moves/nonDecision/effect/requirement/max/moves/MaxMoves.java
@@ -135,41 +135,42 @@ public final class MaxMoves extends Effect
 	 * @param withValue If true, the capture has to maximise the values of the capturing pieces too.
 	 * @return the count of the replay of this move.
 	 */
-	private int getReplayCount(final Context context, final int count, final boolean withValue)
+	private int getReplayCount(final Context contextCopy, final int count, final boolean withValue)
 	{
-		if (context.state().prev() != context.state().mover() || context.trial().over())
+		if (contextCopy.state().prev() != contextCopy.state().mover() || contextCopy.trial().over())
 			return count;
 
-		final Moves legalMoves = context.game().moves(context);
+		final Moves legalMoves = contextCopy.game().moves(contextCopy);
 
 		final int[] replayCount = new int[legalMoves.moves().size()];
 
 		for (int i = 0; i < legalMoves.moves().size(); i++)
 		{
 			final Move newMove = legalMoves.moves().get(i);
-			context.game().apply(context, newMove);
+			final Context newContext = new TempContext(contextCopy);
+			newContext.game().apply(newContext, newMove);
 			if (!withValue)
 			{
-				replayCount[i] = getReplayCount(context, count + 1, withValue);
+				replayCount[i] = getReplayCount(newContext, count + 1, withValue);
 			}
 			else
 			{
 				int numCaptureWithValue = 0;
-				final List<Action> actions = newMove.getActionsWithConsequences(context);
+				final List<Action> actions = newMove.getActionsWithConsequences(contextCopy);
 				for (final Action action : actions)
 				{
 					if (action != null && action.actionType().equals(ActionType.Remove))
 					{
 						final int site = action.to();
 						final SiteType type = action.toType();
-						final ContainerState cs = context.containerState(0);
+						final ContainerState cs = contextCopy.containerState(0);
 						final int value = cs.value(site, type);
 						numCaptureWithValue += value;
 					}
 				}
-				replayCount[i] = getReplayCount(context, count + numCaptureWithValue, withValue);
+				replayCount[i] = getReplayCount(newContext, count + numCaptureWithValue, withValue);
+				
 			}
-			context.game().undo(context);
 		}
 
 		int max = 0;
@@ -183,6 +184,54 @@ public final class MaxMoves extends Effect
 
 		return max;
 	}
+//	private int getReplayCount(final Context context, final int count, final boolean withValue)
+//	{
+//		if (context.state().prev() != context.state().mover() || context.trial().over())
+//			return count;
+//
+//		final Moves legalMoves = context.game().moves(context);
+//
+//		final int[] replayCount = new int[legalMoves.moves().size()];
+//
+//		for (int i = 0; i < legalMoves.moves().size(); i++)
+//		{
+//			final Move newMove = legalMoves.moves().get(i);
+//			context.game().apply(context, newMove);
+//			if (!withValue)
+//			{
+//				replayCount[i] = getReplayCount(context, count + 1, withValue);
+//			}
+//			else
+//			{
+//				int numCaptureWithValue = 0;
+//				final List<Action> actions = newMove.getActionsWithConsequences(context);
+//				for (final Action action : actions)
+//				{
+//					if (action != null && action.actionType().equals(ActionType.Remove))
+//					{
+//						final int site = action.to();
+//						final SiteType type = action.toType();
+//						final ContainerState cs = context.containerState(0);
+//						final int value = cs.value(site, type);
+//						numCaptureWithValue += value;
+//					}
+//				}
+//				replayCount[i] = getReplayCount(context, count + numCaptureWithValue, withValue);
+//			}
+//			context.game().undo(context);
+//		}
+//
+//		int max = 0;
+//
+//		// Get the max of the replayCount.
+//		for (final int nbReplay : replayCount)
+//		{
+//			if (nbReplay > max)
+//				max = nbReplay;
+//		}
+//
+//		return max;
+//	}
 	
 	//-------------------------------------------------------------------------
 	
