51f692d9901ae0638e9acc273ccb0a17a3439f39
DennisSoemers <d.soemers@gmail.com>
2021-11-26 12:49:16 +0100

    use less memory for REINFORCE

--- AI/src/training/policy_gradients/Reinforce.java
+++ AI/src/training/policy_gradients/Reinforce.java
@@ -50,6 +50,9 @@ public class Reinforce
 	/** We don't store experiences for which the discount factor drops below this threshold */
 	private static final double EXPERIENCE_DISCOUNT_THRESHOLD = 0.001;
 	
+	/** If we have a discount factor gamma = 1, we'll use this threshold to limit amount of data stored per trial */
+	private static final int DATA_PER_TRIAL_THRESHOLD = 150;
+	
 	//-------------------------------------------------------------------------
 	
 	/**
@@ -180,6 +183,10 @@ public class Reinforce
 									final BaseFeatureSet featureSet = epochFeatureSets[mover];
 				
 									final FeatureVector[] featureVectors = featureSet.computeFeatureVectors(context, moves, false);
+									for (final FeatureVector featureVector : featureVectors)
+									{
+										featureVector.activeSpatialFeatureIndices().trimToSize();
+									}
 									final FVector distribution = playoutPolicy.computeDistribution(featureVectors, mover);
 									
 									final int moveIdx = playoutPolicy.selectActionFromDistribution(distribution);
@@ -567,21 +574,39 @@ public class Reinforce
 			
 			double discountMultiplier = 1.0;
 			
+			final boolean[] skipData = new boolean[featureVectorsList.size()];
+			if (trainingParams.pgGamma == 1.0)
+			{
+				int numSkipped = 0;
+				while (skipData.length - numSkipped > DATA_PER_TRIAL_THRESHOLD)
+				{
+					final int skipIdx = ThreadLocalRandom.current().nextInt(skipData.length);
+					if (!skipData[skipIdx])
+					{
+						skipData[skipIdx] = true;
+						++numSkipped;
+					}
+				}
+			}
+			
 			for (int i = featureVectorsList.size() - 1; i >= 0; --i)
 			{
-				epochExperiences[p].add
-				(
-					new PGExperience
+				if (!skipData[i])
+				{
+					epochExperiences[p].add
 					(
-						gameStatesList.get(i),
-						lastDecisionMovesList.get(i),
-						legalMovesList.get(i),
-						featureVectorsList.get(i), 
-						moveIndicesList.getQuick(i), 
-						(float)utilities[p],
-						discountMultiplier
-					)
-				);
+						new PGExperience
+						(
+							gameStatesList.get(i),
+							lastDecisionMovesList.get(i),
+							legalMovesList.get(i),
+							featureVectorsList.get(i), 
+							moveIndicesList.getQuick(i), 
+							(float)utilities[p],
+							discountMultiplier
+						)
+					);
+				}
 				
 				discountMultiplier *= trainingParams.pgGamma;
 				
