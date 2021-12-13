6c49b7c2e9e953bb5a934c524c7e66cdf7434df4
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2021-12-13 09:30:42 +0100

    more

--- Mining/src/utils/concepts/script/CreateClusterConceptScript.java
+++ Mining/src/utils/concepts/script/CreateClusterConceptScript.java
@@ -24,15 +24,15 @@ public class CreateClusterConceptScript
 {
 	public static void main(final String[] args)
 	{
-		final int maxTimeMinutesCluster = 600; // 6000
+		final int maxTimeMinutesCluster = 6000; // 6000
 		final int numPlayout = 100;
 		final int maxTime = 175000;
-		final int maxMove = 250; //5000; // Constants.DEFAULT_MOVES_LIMIT;
+		final int maxMove = 5000; //250; //5000; // Constants.DEFAULT_MOVES_LIMIT;
 		final int allocatedMemoryJava = 4096;
 		final int thinkingTime = 1;
-		final String agentName = "UCT"; // Can be "UCT",  "Alpha-Beta", "Alpha-Beta-UCT", "AB-Odd-Even", or "Random"
+		final String agentName = "Random"; // Can be "UCT",  "Alpha-Beta", "Alpha-Beta-UCT", "AB-Odd-Even", or "Random"
 		final String clusterLogin = "ls670643";
-		final String folder = ""; //"/../Trials/TrialsRandom";
+		final String folder = "/../Trials/TrialsRandom"; //"";
 		final String mainScriptName = "StateConcepts.sh";
 		try (final PrintWriter mainWriter = new UnixPrintWriter(new File(mainScriptName), "UTF-8"))
 		{
