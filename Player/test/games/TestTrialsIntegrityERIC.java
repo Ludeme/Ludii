49d50c1c6911eea7e39c0ed8c75d8781dbaab6e3
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-01-10 15:03:58 +0100

    cluster stuff

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Conflagration and Keep-a-riding"))
+			if (fileEntry.getName().contains("Goats Wintering"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
