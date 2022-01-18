7ad1830496fcb0486ed3d21737dba2c4bd14c8b3
matthew <matthew@matthew-ThinkPad-X1-Extreme-Gen-3>
2022-01-18 15:33:56 +0100

    Id metadata added

---
+++ Core/src/metadata/info/database/Id.java
@@ -0,0 +1,54 @@
+package metadata.info.database;
+
+import metadata.info.InfoItem;
+
+//-----------------------------------------------------------------------------
+
+/**
+ * Specifies the database Id for the currently chosen ruleset.
+ * 
+ * @author Matthew.Stephenson
+ */
+public class Id implements InfoItem
+{
+	
+	/** Ruleset Database Id. */
+	private final Integer id;
+
+	//-------------------------------------------------------------------------
+
+	/**
+	 * @param id The ruleset database table Id.
+	 * 
+	 * @example (id 35)
+	 */
+	public Id(final Integer id)
+	{
+		this.id = id;
+	}
+
+	//-------------------------------------------------------------------------
+
+	@Override
+	public String toString()
+	{
+		final StringBuilder sb = new StringBuilder();
+		
+		sb.append("    (id \"" + id + "\")\n");
+		
+		return sb.toString();
+	}
+	
+	//-------------------------------------------------------------------------
+	
+	/**
+	 * @return The ruleset database table Id.
+	 */
+	public Integer id()
+	{
+		return id;
+	}
+	
+	//-------------------------------------------------------------------------
+
+}
