94e6ed57784547aa39e12fd29ba00854a3334995
matthew <matthew@matthew-ThinkPad-X1-Extreme-Gen-3>
2022-01-24 10:48:30 +0100

    eric check

--- Mining/src/gameDistance/datasets/bagOfWords/BooleanConceptDataset.java
+++ Mining/src/gameDistance/datasets/bagOfWords/BooleanConceptDataset.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import game.Game;
 import gameDistance.datasets.Dataset;
 import other.concept.Concept;
+import other.concept.ConceptComputationType;
+import other.concept.ConceptDataType;
 import utils.data_structures.support.zhang_shasha.Tree;
 
 /**
@@ -21,12 +23,24 @@ public class BooleanConceptDataset implements Dataset
 	public Map<String, Double> getBagOfWords(final Game game) 
 	{
 		final Map<String, Double> featureMap = new HashMap<>();
-
+		
 		for (int i = 0; i < Concept.values().length; i++)
 		{
 			final Concept concept = Concept.values()[i];
-			if (game.booleanConcepts().get(concept.id()))
-				featureMap.put(concept.name(), Double.valueOf(1.0));
+			if(concept.computationType().equals(ConceptComputationType.Compilation)) 
+			{
+				if (concept.dataType().equals(ConceptDataType.BooleanData))
+				{
+					if (game.booleanConcepts().get(concept.id()))
+						featureMap.put(concept.name(), Double.valueOf(1.0));
+					else
+						featureMap.put(concept.name(), Double.valueOf(0.0));
+				}
+				else if (concept.dataType().equals(ConceptDataType.DoubleData) || concept.dataType().equals(ConceptDataType.IntegerData))
+				{
+					featureMap.put(concept.name(), Double.valueOf(game.nonBooleanConcepts().get(concept.id())));
+				}
+			}
 		}
 		
 		return featureMap;
