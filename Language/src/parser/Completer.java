316162cb7ab06a7d66c0c73c22cda56662d201b8
Cameron Browne <cambolbro@gmail.com>
2022-04-07 16:28:11 +0200

    Added some reconstruction routines (from Matthew) with tests.

--- Language/src/parser/Completer.java
+++ Language/src/parser/Completer.java
@@ -1,7 +1,17 @@
 package parser;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import main.grammar.Description;
 import main.grammar.Report;
@@ -42,6 +52,108 @@ public class Completer
 		return completions;
 	}
 
+	//-------------------------------------------------------------------------
+
+	/**
+	 * @return Names and contents of all files within the lud/board path.
+	 */
+	public static Map<String, String> getAllLudContents()
+	{
+		return getAllDirectoryContents("../Common/res/lud/board/");
+	}
+	
+	/**
+	 * @return Names and contents of all files within the define path.
+	 */
+	public static Map<String, String> getAllDefContents()
+	{
+		return getAllDirectoryContents("../Common/res/def/");
+	}
+	
+	/**
+	 * @return Names and contents of all files within the specific directory path.
+	 */
+	public static Map<String, String> getAllDirectoryContents(final String dir)
+	{
+		final File startFolder = new File(dir);
+		final List<File> gameDirs = new ArrayList<>();
+		gameDirs.add(startFolder);
+		final Map<String, String> fileContents = new HashMap<>();
+
+		for (int i = 0; i < gameDirs.size(); ++i)
+		{
+			final File gameDir = gameDirs.get(i);
+
+			for (final File fileEntry : gameDir.listFiles())
+			{
+				if (fileEntry.isDirectory())
+				{
+					gameDirs.add(fileEntry);
+				}
+				else
+				{
+					try(BufferedReader br = new BufferedReader(new FileReader(fileEntry))) 
+					{
+					    final StringBuilder sb = new StringBuilder();
+					    String line = br.readLine();
+
+					    while (line != null) 
+					    {
+					        sb.append(line);
+					        sb.append(System.lineSeparator());
+					        line = br.readLine();
+					    }
+					    
+					    final String everything = sb.toString();
+					    fileContents.put(fileEntry.getName(), everything);
+					} 
+					catch (final FileNotFoundException e) 
+					{
+						e.printStackTrace();
+					} 
+					catch (final IOException e) 
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+		return fileContents;
+	}
+
+	//-------------------------------------------------------------------------
+	
+	/**
+	 * Print game options per .lud to file.
+	 * @param fileName
+	 * @throws IOException 
+	 */
+	public static void saveReconstruction
+	(
+		final String name, final String content
+	) throws IOException
+	{
+		final String outFileName = "../Common/res/out/recons/" + name + ".lud";	
+		
+		// Prepare the output file
+		final File file = new File(outFileName);
+		if (!file.exists())
+			file.createNewFile();
+
+		try 
+		(
+			final PrintWriter writer = 
+				new PrintWriter
+				(
+					new BufferedWriter(new FileWriter(outFileName, false))
+				)
+		)
+		{
+			writer.write(content);
+		}
+	}
+	
 	//-------------------------------------------------------------------------
 
 }
---
+++ Language/src/test/TestReconstruction.java
@@ -0,0 +1,62 @@
+package test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import parser.Completer;
+
+//-----------------------------------------------------------------------------
+
+/**
+ * Test various reconstruction routines.
+ * @author cambolbro
+ */
+class TestReconstruction
+{
+	
+	//-------------------------------------------------------------------------
+
+	void testSaving()
+	{
+		try
+		{
+			Completer.saveReconstruction("Test", "(test ...)");
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+		
+	//-------------------------------------------------------------------------
+
+	void testLoadLuds()
+	{
+		final Map<String, String> luds = Completer.getAllLudContents();
+		final Map<String, String> defs = Completer.getAllDefContents();
+		
+		System.out.println(luds.size() + " luds loaded, " + defs.size() + " defs loaded.");
+	}
+	
+	//-------------------------------------------------------------------------
+
+	@Test
+	public void test()
+	{
+		testSaving();
+		testLoadLuds();
+	}
+
+//	public static void main(String[] args)
+//	{
+//		final TestReconstruction app = new TestReconstruction();
+//		
+//		app.testSaving();
+//		app.testLoadLuds();
+//	}
+	
+}
