e5011497d68846519c92dfd75641089b51134b03
Alied Perez <alied@xcodingworld.com>
2022-05-21 16:28:34 +0100

    restore RandomGameTester

--- Generation/test/RandomGameTester.java
+++ Generation/test/RandomGameTester.java
@@ -1,5 +1,4 @@
 import approaches.random.Generator;
-import org.junit.Test;
 
 /**
  * Random game tester.
@@ -7,8 +6,7 @@ import org.junit.Test;
  */
 public class RandomGameTester
 {
-    @Test
-	public void testGames()
+	public static void main(final String[] arg)
 	{
 		Generator.testGames(1000, true, false, false, false);
 	}
