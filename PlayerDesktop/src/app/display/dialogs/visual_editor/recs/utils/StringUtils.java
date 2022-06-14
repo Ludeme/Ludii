aebcf4ce41f43e3fdd0622d4b9164a135d593fb0
Filip <fil.rehburg@gmail.com>
2022-06-14 17:50:03 +0200

    Changed preprocessing to use tokentree

--- PlayerDesktop/src/app/display/dialogs/visual_editor/recs/utils/StringUtils.java
+++ PlayerDesktop/src/app/display/dialogs/visual_editor/recs/utils/StringUtils.java
@@ -28,4 +28,54 @@ public class StringUtils {
         }
         return s;
     }
+
+    public static String charsBeforeCursor(String text, int caretPosition)
+    {
+        if (text.isEmpty()) return "";
+
+        final int pos = caretPosition;
+        int start = pos-1;
+        try {
+            while (start > 0 && Character.isLetterOrDigit(getSubstring(text,start-1,1).charAt(0)))
+                start--;
+
+            final String result = getSubstring(text,Math.max(0,start),pos-start);
+            return result;
+        }
+        catch (final Exception e)
+        {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * get the substring that starts at index offs with a length of len
+     * @param s
+     * @param offs
+     * @param len
+     * @return
+     */
+    public static String getSubstring(String s, int offs, int len) {
+        int len2;
+        if(offs + len == s.length() + 1) {
+             len2 = len - 1;
+        } else {
+            len2 = len;
+        }
+        String substring = s.substring(offs, offs+len2);
+        return substring;
+    }
+
+    public static String repeat(String s, int repetitions) {
+        if(repetitions <= 0) {
+            return "";
+        } else {
+            String t = "";
+            for(int i = 0; i < repetitions; i++) {
+                t += s;
+            }
+            return t;
+        }
+    }
 }
