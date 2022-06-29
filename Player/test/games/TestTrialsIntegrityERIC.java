175bdf984923af9f0046aef727c9a5f4db76d9c9
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-06-29 12:09:50 +0200

    many shorter descs

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Haretavl"))
+			if (fileEntry.getName().contains("Jeu de Renard"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
