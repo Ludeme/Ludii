34942acb3e28229d8c06475f013c375c125b2804
matthew <matthew@matthew-ThinkPad-X1-Extreme-Gen-3>
2022-06-30 10:40:23 +0200

    play and test buttons combined

--- Player/src/app/PlayerApp.java
+++ Player/src/app/PlayerApp.java
@@ -85,8 +85,6 @@ public abstract class PlayerApp implements PlayerInterface, ActionListener, Item
 	public abstract void showOtherDialog(FastArrayList<Move> otherPossibleMoves);
 	public abstract void showInfoDialog();
 	
-	public abstract void checkButtonsEnabled();
-	
 	public abstract int width();
 	public abstract int height();
 	
