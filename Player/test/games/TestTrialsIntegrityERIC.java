f1cdb15866f5ba5b2a85a3abc4bdbca954264cd7
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-02 12:20:20 +0200

    Mao Naga... shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Manu"))
+			if (fileEntry.getName().contains("Mao Naga Tiger Game"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
