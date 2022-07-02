4823661c04ab021f634f552407a1ee86aa5a2ec9
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-02 12:26:22 +0200

    Musinaykahwhanmetowaywin shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Mao Naga Tiger Game"))
+			if (fileEntry.getName().contains("Musinaykahwhanmetowaywin"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
