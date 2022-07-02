fab3da221d27ca204fa8c3558d4d615e80afb8e9
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-02 13:44:53 +0200

    All hunt games shorter descs

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Shi Liu Kan Tsiang Kun"))
+			if (fileEntry.getName().contains("Yeung Luk Sz' Kon Tseung Kwan"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
