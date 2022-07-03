7638d316bf9359df88dc598769401c98df1a0b2c
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-03 14:22:40 +0200

    Better ludemplexes related to Chess pieces

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("58 Holes"))
+			if (fileEntry.getName().contains("Asi Keliya"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
