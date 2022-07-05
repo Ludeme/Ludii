2141038551e90456cc92d1cd6307a56f0330171d
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-05 09:41:21 +0200

    Kotra shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Kiz Tavlasi"))
+			if (fileEntry.getName().contains("Kotra"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
