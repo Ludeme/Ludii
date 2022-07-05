b359dc0d070a2a5edfef5118ca1f571153b3ccb2
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-05 14:34:07 +0200

    Medio Emperador shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Lange Puff"))
+			if (fileEntry.getName().contains("Medio Emperador"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
