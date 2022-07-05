537fbefadd919d6cac04dc37160f95428a43cddd
Filipp Dokienko <fmelenteva@gmail.com>
2022-07-05 19:13:17 +0200

    Update DescriptionGraph.java

--- PlayerDesktop/src/app/display/dialogs/visual_editor/model/DescriptionGraph.java
+++ PlayerDesktop/src/app/display/dialogs/visual_editor/model/DescriptionGraph.java
@@ -7,49 +7,72 @@ import app.display.dialogs.visual_editor.model.interfaces.iGraph;
 import main.grammar.Description;
 import main.grammar.Symbol;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Graph of LudemeNode objects
  * @author Filipp Dokienko
  */
 
-public class DescriptionGraph implements iGraph {
+public class DescriptionGraph implements iGraph
+{
+    /** The Root node of this graph */
+    private LudemeNode ROOT;
+    /** All nodes this graph contains keyed by their id */
+    private final HashMap<Integer, iGNode> nodeMap = new HashMap<>();
+    /** List of all LudemeNodes this graph contains */
+    private final List<LudemeNode> allLudemeNodes = new ArrayList<>();
+    /** List of all Edges this graph contains */
+    private final List<Edge> edgeList = new ArrayList<>();
 
-    //TODO: change to the HashMap
-    List<LudemeNode> allLudemeNodes = new ArrayList<>();
-    HashMap<Integer, iGNode> nodeMap = new HashMap<>();
-    List<Edge> edgeList = new ArrayList<>();
     private final List<Integer> connectedComponentRoots = new ArrayList<>();
     private int selectedRoot = -1;
+
+    /** Whether this graph is a define */
     private boolean isDefine = false;
+    /** The title of the define */
     private String title;
-    private Symbol symbol;
-    LudemeNode ROOT;
+    /** The symbol of the define */
+    private Symbol defineSymbol;
+    /** The macro node of the define */
+    private LudemeNode defineMacroNode;
+    /** The parameters of the define */
+    private List<NodeArgument> defineParameters = new ArrayList<>();
+    /** The define node to be used in other graphs, to access the define */
+    private LudemeNode defineNode;
 
 
+    /**
+     * Constructor. Used for non-define graphs without a predefined root node
+     */
     public DescriptionGraph()
     {
+
     }
+
+    /**
+     * Constructor for a define graph.
+     * @param title Initial title of the define
+     * @param isDefine Whether this graph is a define
+     */
     public DescriptionGraph(String title, boolean isDefine)
     {
+        assert isDefine;
         this.title = title;
-        this.isDefine = isDefine;
-        this.symbol = new Symbol(Symbol.LudemeType.Ludeme, title, title, null);
+        this.isDefine = true;
+        this.defineSymbol = new Symbol(Symbol.LudemeType.Ludeme, title, title, null);
     }
 
-    public DescriptionGraph(LudemeNode root){
+    /**
+     * Constructor for a non-define graph with a predefined root node
+     * @param root The root node
+     */
+    public DescriptionGraph(LudemeNode root)
+    {
         this.ROOT = root;
-    }
-
-    public String title()
-    {
-        return title;
-    }
-
-    public void setTitle(String title)
-    {
-        this.title = title;
     }
 
     public Description description()
@@ -72,28 +95,12 @@ public class DescriptionGraph implements iGraph {
     @Override
     public LudemeNode getNode(int id)
     {
-        for(LudemeNode ln : allLudemeNodes){
-            if(ln.id() == id) return ln;
-        }
-        return null;
+        return (LudemeNode) nodeMap.get(id);
     }
 
-    public List<LudemeNode> getNodes() {
+    public List<LudemeNode> getNodes()
+    {
         return allLudemeNodes;
-    }
-
-    public List<LudemeNode> getNodes(Symbol symbol)
-    {
-        List<LudemeNode> result = new ArrayList<>();
-        for(LudemeNode ln : allLudemeNodes) if(ln.symbol() == symbol) result.add(ln);
-        return result;
-    }
-
-    public List<LudemeNode> getNodes(String symbolName)
-    {
-        List<LudemeNode> result = new ArrayList<>();
-        for(LudemeNode ln : allLudemeNodes) if(Objects.equals(ln.symbol().name(), symbolName)) result.add(ln);
-        return result;
     }
 
     @Override
@@ -120,7 +127,7 @@ public class DescriptionGraph implements iGraph {
     {
         iGNode node = getNode(id);
         nodeMap.remove(id);
-        this.allLudemeNodes.remove((LudemeNode) node);
+        this.allLudemeNodes.remove(node);
         removeConnectedComponentRoot(node.id());
         return node.id();
     }
@@ -130,9 +137,8 @@ public class DescriptionGraph implements iGraph {
     {
         Edge e = new Edge(from, to);
         for(Edge edge : edgeList)
-        {
-            if(edge.equals(e)) return;
-        }
+            if(edge.equals(e))
+                return;
         edgeList.add(new Edge(from , to));
         removeConnectedComponentRoot(to);
     }
@@ -141,26 +147,23 @@ public class DescriptionGraph implements iGraph {
     public void removeEdge(int from, int to)
     {
         for(Edge e : edgeList)
-        {
             if(e.getNodeA() == from && e.getNodeB() == to)
             {
                 edgeList.remove(e);
                 addConnectedComponentRoot(to);
                 return;
             }
-        }
     }
 
     @Override
-    public void removeEdge(int containsId) {
+    public void removeEdge(int containsId)
+    {
         for(Edge e : new ArrayList<>(edgeList))
-        {
             if(e.getNodeA() == containsId ||e.getNodeB() == containsId)
             {
                 edgeList.remove(e);
                 addConnectedComponentRoot(e.getNodeB());
             }
-        }
     }
 
     @Override
@@ -172,13 +175,15 @@ public class DescriptionGraph implements iGraph {
     @Override
     public void addConnectedComponentRoot(int root)
     {
-        if (!connectedComponentRoots.contains(root) && getNode(root) != null) connectedComponentRoots.add(root);
+        if (!connectedComponentRoots.contains(root) && getNode(root) != null)
+            connectedComponentRoots.add(root);
     }
 
     @Override
     public void removeConnectedComponentRoot(int root)
     {
-        if (connectedComponentRoots.contains(root)) connectedComponentRoots.remove((Object)root);
+        if (connectedComponentRoots.contains(root))
+            connectedComponentRoots.remove((Object)root);
     }
 
     @Override
@@ -194,19 +199,31 @@ public class DescriptionGraph implements iGraph {
     }
 
     @Override
-    public void setRoot(iGNode root) {
+    public void setRoot(iGNode root)
+    {
         this.ROOT = (LudemeNode) root;
     }
 
     @Override
-    public iGNode getRoot() {
+    public iGNode getRoot()
+    {
         return ROOT;
     }
 
-    public void remove(LudemeNode ludemeNode) {
+    public LudemeNode rootLudemeNode()
+    {
+        return (LudemeNode) ROOT;
+    }
+
+    public void remove(LudemeNode ludemeNode)
+    {
         this.allLudemeNodes.remove(ludemeNode);
     }
 
+    /**
+     * Converts the graph to a .lud String
+     * @return The .lud equivalent of the graph
+     */
     public String toLud()
     {
         if(!isDefine)
@@ -215,41 +232,169 @@ public class DescriptionGraph implements iGraph {
             return defineLud();
     }
 
+
+    // Methods for define graphs
+
+
+    /**
+     * Converts a define graph to a .lud String.
+     * Replaces unprovided, but required arguments, indicated by " <PARAMETER> " with #[count]
+     * @return The .lud equivalent of the define graph
+     */
     private String defineLud()
     {
         String lud = ((LudemeNode) getRoot()).toLud(true);
         int count = 1;
         while(lud.contains("<PARAMETER>"))
         {
-            lud = lud.replaceFirst("<PARAMETER>","#"+Integer.toString(count));
+            lud = lud.replaceFirst("<PARAMETER>","#"+count);
             count++;
         }
         return lud;
     }
 
+    /**
+     *
+     * @return Whether this is a define graph
+     */
     public boolean isDefine()
     {
         return isDefine;
     }
 
+    /**
+     *
+     * @return The title of the define
+     */
+    public String title()
+    {
+        return title;
+    }
+
+    /**
+     * Changes the title of the define
+     * @param title The new title
+     */
+    public void setTitle(String title)
+    {
+        if(!title.equals(this.title))
+        {
+            this.title = title;
+            // update the symbol
+            this.defineSymbol = new Symbol(Symbol.LudemeType.Ludeme, title, title, null);
+            // TODO: Notify about change
+            System.out.println("[NOTIFY] Title changed");
+        }
+    }
+
+    /**
+     *
+     * @return The symbol of the define
+     */
+    public Symbol defineSymbol()
+    {
+        return defineSymbol;
+    }
+
+    /**
+     *
+     * @return The macro node (root of the define (excluding the define node itself)) of the define
+     */
+    public LudemeNode defineMacroNode()
+    {
+        return defineMacroNode;
+    }
+
+    /**
+     *
+     * @return The current macro node of the define
+     */
+    public LudemeNode computeDefineMacroNode()
+    {
+        return (LudemeNode) rootLudemeNode().providedInputsMap().get(rootLudemeNode().currentNodeArguments().get(1));
+    }
+
+    /**
+     * Updates the field of the define macro node
+     */
+    public void updateMacroNode()
+    {
+        // if the macro node has changed, update it and notify about change
+        if(defineMacroNode != computeDefineMacroNode())
+        {
+            defineMacroNode = computeDefineMacroNode();
+            System.out.println("[NOTIFY] Macro node changed to " + defineMacroNode.title());
+            // TODO: Notify
+        }
+    }
+
+    public List<NodeArgument> parameters()
+    {
+        return defineParameters;
+    }
+
+    /**
+     * Computes the required arguments of the define
+     * @return List of NodeArguments that require an Input
+     */
+    public List<NodeArgument> computeDefineParameters()
+    {
+        assert isDefine;
+        List<NodeArgument> parameters = new ArrayList<>();
+        LudemeNode root = (LudemeNode) getRoot();
+        // The node must be satisfied (means all inputs filled)
+        if(!root.isSatisfied())
+            return parameters;
+
+        for(LudemeNode node : allLudemeNodes)
+            parameters.addAll(node.unfilledRequiredArguments());
+
+        return parameters;
+    }
+
+    /**
+     * Updates the current parameters of the define.
+     * Returns which NodeArguments were added/removed from the parameters (of a define node)
+     * @return An array of two List<NodeArgument>. The first list contains newly added NodeArguments, the second list contains removed NodeArguments.
+     */
+    public Object[] changedParameters(List<NodeArgument> parameters)
+    {
+        assert isDefine;
+        if(this.defineParameters.isEmpty())
+        {
+            this.defineParameters = parameters;
+            return new Object[]{new ArrayList<>(), new ArrayList<>()};
+        }
+        List<NodeArgument> added = new ArrayList<>();
+        List<NodeArgument> removed = new ArrayList<>(this.defineParameters);
+
+        for(NodeArgument na : parameters)
+            if(!parameters().contains(na))
+                added.add(na);
+            else
+                removed.remove(na);
+
+        this.defineParameters = parameters;
+
+        if(!added.isEmpty() || !removed.isEmpty())
+        {
+            System.out.println("[NOTIFY] Parameters changed");
+            // TODO: Notify about change
+        }
+
+        return new Object[]{added, removed};
+    }
+
+    /**
+     *
+     * @return The node of the define, used in other graphs
+     */
     public LudemeNode defineNode()
     {
         assert isDefine;
-        LudemeNode lnRoot = (LudemeNode) getRoot();
-        if(!lnRoot.isSatisfied())
-            return null;
-        Map<LudemeNode, List<NodeArgument>> parameters = Handler.defineParameters(this);
-        List<NodeArgument> parameterNAs = new ArrayList<>();
-        for(List<NodeArgument> lna : parameters.values()) parameterNAs.addAll(lna);
-        System.out.println(lnRoot.providedInputsMap().values());
-        String t = lnRoot.providedInputsMap().values().iterator().next().toString();
-        if(!t.equals(title))
-        {
-            title = t;
-            symbol = new Symbol(Symbol.LudemeType.Ludeme, t, t, null);
-        }
-        LudemeNode macroNode = (LudemeNode) lnRoot.providedInputsMap().get(lnRoot.currentNodeArguments().get(1));
-        return new LudemeNode(symbol, macroNode, this, parameterNAs, lnRoot.x(), lnRoot.y(), true);
+        if(defineNode == null)
+            defineNode = new LudemeNode(defineSymbol, defineMacroNode(), this, parameters(), rootLudemeNode().x(), rootLudemeNode().y(), true);
+        return defineNode;
     }
 
 }
