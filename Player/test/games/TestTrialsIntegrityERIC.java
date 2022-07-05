c2721a268152f95fb3a6908acb85d1a600e8b6fa
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-05 17:44:07 +0200

    Portes shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Petol"))
+			if (fileEntry.getName().contains("Portes"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
