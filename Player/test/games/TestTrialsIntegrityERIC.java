ff98b836e3a0caff25f2a20607c9c8c34ac86e7b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-05 15:07:39 +0200

    Nard shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Medio Emperador"))
+			if (fileEntry.getName().contains("Nard"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
