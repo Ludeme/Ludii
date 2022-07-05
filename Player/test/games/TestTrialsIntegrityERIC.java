c3148a42af5d9ebad64ccaf0b2ecd78747918a0d
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-05 17:51:33 +0200

    provincial shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Portes"))
+			if (fileEntry.getName().contains("Provincial"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
