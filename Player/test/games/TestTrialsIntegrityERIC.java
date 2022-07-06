93b72d405caa9e69a2ea99ae25fff8573bfb03a2
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-06 09:55:24 +0200

    Schuster shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Ghingskoot"))
+			if (fileEntry.getName().contains("Schuster"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
