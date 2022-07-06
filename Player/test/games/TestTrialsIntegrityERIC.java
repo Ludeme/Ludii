fc4c36c6ce461258730e4ed31bf3058631e3c732
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-06 17:11:59 +0200

    Todas Tablas shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tawula"))
+			if (fileEntry.getName().contains("Todas Tablas"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
