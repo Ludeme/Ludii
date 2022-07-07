6c78571be2ae6ace3805eb5ef99078d942ecc8e1
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-07 14:48:09 +0200

    IsPieceAt used in some I did before

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tandems"))
+			if (fileEntry.getName().contains("Salta"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
