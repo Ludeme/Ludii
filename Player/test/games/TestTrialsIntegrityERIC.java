ec73fc7e52f3f9de780b4ce341aab589576c6949
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-08 14:26:30 +0200

    All Reach race games are now shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Sokkattan"))
+			if (fileEntry.getName().contains("Tugi-Epfe"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
