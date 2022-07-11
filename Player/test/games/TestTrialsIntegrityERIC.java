16030fc2712d37ae0b8aca69c9c1f3fdb63a7d7d
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-11 15:37:13 +0200

    more Mancala games shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -108,14 +108,14 @@ public class TestTrialsIntegrityERIC
 		}
 		
 		boolean gameReached = false;
-		final String gameToReached = "Dongjintian";
+		final String gameToReached = "";
 		final String gameToSkip = "";
 
 		final long startTime = System.currentTimeMillis();
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains(""))
+			if (fileEntry.getName().contains("Kara"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
