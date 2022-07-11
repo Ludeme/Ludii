f5c47e0e51ad1833693187050d379a4281490aca
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-11 14:04:05 +0200

    more 2 rows shorter and faster

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Ceelkoqyuqkoqiji"))
+			if (fileEntry.getName().contains("Galatjang"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
