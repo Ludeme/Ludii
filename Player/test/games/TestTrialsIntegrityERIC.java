91f3312a19e2734fcea87046d8a07565596ce865
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-13 15:12:52 +0200

    All blocking games shorter (and sometimes more efficient)

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Do Guti"))
+			if (fileEntry.getName().contains("TacTix"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
