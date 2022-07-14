39b4d7e77980b823d95701e8490239916f734634
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-14 14:55:36 +0200

    Line3Win ludemeplex added

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Dig Dig"))
+			if (fileEntry.getName().contains("Djara-Badakh"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
