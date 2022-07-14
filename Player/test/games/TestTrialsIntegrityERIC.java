be9d73dd5f00243f13175640e439f4d8e08848bb
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-14 08:51:26 +0200

    Some new ludemeplexes and all group games shorter/more efficient

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tara"))
+			if (fileEntry.getName().contains("Groups"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
