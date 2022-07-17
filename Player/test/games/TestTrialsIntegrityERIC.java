6d79d251695cafc1de2ef46a4de327dca4087a45
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-07-17 12:53:09 +0200

    fix Travis

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Weiqi"))
+			if (fileEntry.getName().contains("HexGo"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
