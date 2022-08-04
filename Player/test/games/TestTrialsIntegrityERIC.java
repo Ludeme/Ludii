2e951dc2094fe1619539b067707575fcea54c0eb
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-04 15:29:56 +0200

    Dum Blas a lot shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Doov"))
+			if (fileEntry.getName().contains("Dum Blas"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
