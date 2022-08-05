9a7f77661274f453e241122bdc1b33fbbf318e4e
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-05 16:35:42 +0200

    Hewakam Keliya shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Game of Solomon"))
+			if (fileEntry.getName().contains("Hewakam Keliya"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
