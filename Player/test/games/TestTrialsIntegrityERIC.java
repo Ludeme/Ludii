817a99dfab4571423273a8eee3c62708c4756820
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-05 14:33:08 +0200

    Challis Ghutia (Titagarh) shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Buudal"))
+			if (fileEntry.getName().contains("Challis Ghutia (Titagarh)"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
