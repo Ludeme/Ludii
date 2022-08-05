08e2ee6997b7fea1c5abcb2ef8308d450c93d1a1
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-05 16:04:14 +0200

    Dam Hariman board done with ludemeplex

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Dama (Alquerque)"))
+			if (fileEntry.getName().contains("Dam Hariman"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
