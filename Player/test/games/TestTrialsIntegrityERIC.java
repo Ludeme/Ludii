45dcf746e27dfc87b580875505c55922e1a2571e
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-05 12:54:36 +0200

    All Leaping diagonal done with new ludemeplexes

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("The Babylonian"))
+			if (fileEntry.getName().contains("Unnamed Dutch Draughts Game"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
