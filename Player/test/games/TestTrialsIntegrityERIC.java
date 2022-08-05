f062ef42e55aa5892c0d3bc015191281ebede222
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-05 15:22:54 +0200

    Dablot Prejjesne a bit shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Crand"))
+			if (fileEntry.getName().contains("Dablot Prejjesne"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
