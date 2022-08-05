b6383f5fbd48adf9644558d05c77c9bedac41ce5
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-05 11:21:09 +0200

    Seesaw Draughts tiny improvement

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Pleasant Draughts"))
+			if (fileEntry.getName().contains("Seesaw Draughts"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
