11a6e24afcc5fe7193a92cb8656dece3da245e6d
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 14:42:16 +0200

    Seesaw shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Queah Game"))
+			if (fileEntry.getName().contains("Seesaw"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
