1db40d3a30c2e16ca58621aaa67365168f8d60fa
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 10:47:19 +0200

    Ratti-Chitti-Bakri shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Ram Tir"))
+			if (fileEntry.getName().contains("Ratti-Chitti-Bakri"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
