5d88a1832faa63401a93a87cb1b8ad5406b6e4d2
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 14:50:01 +0200

    Tablo shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Seesaw"))
+			if (fileEntry.getName().contains("Tablo"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
