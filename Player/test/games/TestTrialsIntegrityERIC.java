490c4d73015c7c54a14ca127c79f8b95f475d4b0
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 10:29:16 +0200

    Mughal Pathan shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Mogul Putt'han"))
+			if (fileEntry.getName().contains("Mughal Pathan"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
