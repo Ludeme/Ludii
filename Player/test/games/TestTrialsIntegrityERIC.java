262f0df27cc30cfeca43faac960771d284fabf92
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 15:10:03 +0200

    Xarajlt shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tre Guti"))
+			if (fileEntry.getName().contains("Xarajlt"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
