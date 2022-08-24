723f0d17fa2b6d2b2f4056791d225aefa18e2f42
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 10:09:18 +0200

    La Dama shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Kotu Ellima"))
+			if (fileEntry.getName().contains("La Dama"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
