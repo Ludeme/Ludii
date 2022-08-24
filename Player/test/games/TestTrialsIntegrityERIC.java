cf1d8c5df82d9050fec1dc511816bab82652a5fd
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 17:07:36 +0200

    Chaturanga (Kridakausalya) shorter desc + fix rook

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Chaturanga (al-Adli)"))
+			if (fileEntry.getName().contains("Chaturanga (Kridakausalya)"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
