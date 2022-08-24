97c61a5deae38b179b916878d6dc1729233a45d7
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 10:52:29 +0200

    Tavelspel shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Sumi Naga Game (War)"))
+			if (fileEntry.getName().contains("Tavelspel"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
