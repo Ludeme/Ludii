f744d1be529547b413c33fc94c56f4eaf09f3ef9
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 14:31:45 +0200

    Natt Klab ash-Shawk shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Mraha wa Bwe"))
+			if (fileEntry.getName().contains("Natt Klab ash-Shawk"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
