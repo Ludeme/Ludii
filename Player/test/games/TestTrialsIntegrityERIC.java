c98c3423a555fdd98a21292c53089145049d9c98
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 10:39:39 +0200

    Pam Pait shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Mughal Pathan"))
+			if (fileEntry.getName().contains("Pam Pait"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
