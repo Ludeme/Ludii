4e93d32d08eaab47372e5e91d0790068aea6af29
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 14:21:08 +0200

    Laram Wali shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Koruboddo"))
+			if (fileEntry.getName().contains("Laram Wali"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
