4df176990d29b8acefd0409d9536fdbf14b9043f
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 16:55:18 +0200

    Chaturanga (14x14) shorter desc + fix for horses

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Chaturanga (12x12)"))
+			if (fileEntry.getName().contains("Chaturanga (14x14)"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
