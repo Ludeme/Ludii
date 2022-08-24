84a68f544132018cec12310e72ef1a69ffce8e4a
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-24 11:17:20 +0200

    Tuknanavuhpi minor

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tsukkalavde"))
+			if (fileEntry.getName().contains("Tuknanavuhpi"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
