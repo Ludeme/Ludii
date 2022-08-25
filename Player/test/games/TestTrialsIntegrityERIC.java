697e1bc590ada6dc883da1fe9bd446613509f91b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-25 11:25:08 +0200

    Main Chator (Selangor) shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Main Chator (Batak)"))
+			if (fileEntry.getName().contains("Main Chator (Selangor)"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
