a7032b6ecf0a8ce27a7d69284b3d8e168664b96a
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-25 12:04:56 +0200

    Parsi Chess shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Ouk Chatrang"))
+			if (fileEntry.getName().contains("Parsi Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
