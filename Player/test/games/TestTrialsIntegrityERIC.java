19624e45ea3d82ed46a2ca45ce0921f4c8dae35a
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-25 10:03:24 +0200

    Cittabhramanrpasya Khelanam shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -115,7 +115,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Chaturanga (Kridakausalya 14x14)"))
+			if (fileEntry.getName().contains("Cittabhramanrpasya Khelanam"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
