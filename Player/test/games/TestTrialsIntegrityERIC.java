ee90d60b34519131b2cb6ad9fd387ca83e8f44eb
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 15:01:33 +0200

    Shatranj at-Tamma shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Shatranj ar-Rumiya"))
+			if (fileEntry.getName().contains("Shatranj at-Tamma"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
