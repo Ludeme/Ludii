6364e984a596b44c056b2796b9afa306e1163008
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 16:10:09 +0200

    Tepong shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Sittuyin"))
+			if (fileEntry.getName().contains("Tepong"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
