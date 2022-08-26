f0f7a00bcef6aa44bc6e8f8d4aec2c2a84728972
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 13:13:13 +0200

    Shatranj (12x12) shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Shatranj"))
+			if (fileEntry.getName().contains("Shatranj (12x12)"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
