467d0baf5f20dfec8fdc972e369d6e89df5123ca
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 12:56:34 +0200

    Shataranja shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Saxun"))
+			if (fileEntry.getName().contains("Shataranja"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
