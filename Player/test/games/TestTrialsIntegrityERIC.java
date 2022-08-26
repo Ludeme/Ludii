9f6f95f754516224dd5362fdf7f4d85ef14829c4
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 15:10:00 +0200

    Shatranj Diwana Shah shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Shatranj at-Tamma"))
+			if (fileEntry.getName().contains("Shatranj Diwana Shah"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
