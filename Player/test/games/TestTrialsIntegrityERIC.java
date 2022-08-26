f16fe74e5e4adef6800b5bdbb830267a84292c43
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 16:24:55 +0200

    1D Chess shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tsatsarandi"))
+			if (fileEntry.getName().contains("1D Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
