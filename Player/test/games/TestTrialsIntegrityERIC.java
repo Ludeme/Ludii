874966ee9bb7172cda3f767bf2f85a5ab5469cc7
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 16:29:31 +0200

    Acedrex (Alfonso) shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("1D Chess"))
+			if (fileEntry.getName().contains("Acedrex (Alfonso)"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
