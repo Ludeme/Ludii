aa503f78af075a838d4639f0a1c0bdae8ac536be
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 14:41:48 +0200

    Shatranj al-Mustatila shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Shatranj al-Kabir"))
+			if (fileEntry.getName().contains("Shatranj al-Mustatila"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
