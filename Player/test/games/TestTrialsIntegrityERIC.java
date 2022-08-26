4b64886ded0916960143c5a8778f6c86c214fcfb
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-26 12:03:29 +0200

    Sarvatobhadra shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Santaraj"))
+			if (fileEntry.getName().contains("Sarvatobhadra"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
