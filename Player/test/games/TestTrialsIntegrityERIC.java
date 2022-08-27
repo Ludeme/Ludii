488cfef59c140a64eb96bec5e7e1507ce54314e2
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-27 15:40:00 +0200

    Chessence a little bit shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Brusky Chess"))
+			if (fileEntry.getName().contains("Chessence"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
