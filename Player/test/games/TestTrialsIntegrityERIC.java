bf4bde4638f224cc017214d0c379c76a1ebd7d18
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-27 22:09:35 +0200

    Horde Chess shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Half Chess"))
+			if (fileEntry.getName().contains("Horde Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
