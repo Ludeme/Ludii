5a3fcc378db040da7f01846e20dc910b749eccc7
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-27 21:56:59 +0200

    Grande Acedrex shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Glinski Chess"))
+			if (fileEntry.getName().contains("Grande Acedrex"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
