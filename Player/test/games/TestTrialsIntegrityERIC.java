b0734e561ed483e90e8570e36a712d06e7324b26
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-27 18:28:36 +0200

    Fischer Random Chess shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Dragonchess"))
+			if (fileEntry.getName().contains("Fischer Random Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
