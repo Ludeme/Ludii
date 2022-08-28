3ea43188f77c93bb12124a4b22f955094bcf5dd2
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-28 10:28:47 +0200

    Long Assize shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Lombard Chess"))
+			if (fileEntry.getName().contains("Long Assize"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
