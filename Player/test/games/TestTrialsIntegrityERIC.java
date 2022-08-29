da97949043657031914f92758b9013d58bb774b3
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-29 15:48:12 +0200

    Quick Chess shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Phase Chess"))
+			if (fileEntry.getName().contains("Quick Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
