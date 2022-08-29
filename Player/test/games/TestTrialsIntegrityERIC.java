ee39fe8f5f41347efff482e19ff35d48205651c6
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-29 15:50:55 +0200

    Russian Fortress Chess slightly shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Quick Chess"))
+			if (fileEntry.getName().contains("Russian Fortress Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
