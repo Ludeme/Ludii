c04447a8eb4f1185ed99b219b1889588a8c1b592
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-29 15:35:08 +0200

    Medieval Chess shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("McCooey Chess"))
+			if (fileEntry.getName().contains("Medieval Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
