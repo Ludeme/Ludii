e2b51c59cf83c2370fe381d98b32db087c7eeffa
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-29 16:27:29 +0200

    StarChess slightly shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Shafran Chess"))
+			if (fileEntry.getName().contains("Starchess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
