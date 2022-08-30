5bca1e136b1a8f0caf5792c64867667abb451acc
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 14:04:43 +0200

    Unashogi shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tobi Shogi"))
+			if (fileEntry.getName().contains("Unashogi"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
