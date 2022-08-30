cc94c76a962c456fc2a6615b187273473809c268
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 14:00:20 +0200

    Tenjiku Shogi shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tai Shogi"))
+			if (fileEntry.getName().contains("Tenjiku Shogi"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
