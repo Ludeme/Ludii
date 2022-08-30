cb0a0491145616069ffe19d4e9f34c78fd7e30a7
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:02:05 +0200

    Banqi shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Awangdu"))
+			if (fileEntry.getName().contains("Banqi"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
