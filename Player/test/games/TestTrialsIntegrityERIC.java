74afaa5c10a8211be85e18821d03a15c753eb8f1
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:28:44 +0200

    Daldosa shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Daldos"))
+			if (fileEntry.getName().contains("Daldosa"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
