8de3a5ea7c5177e868e8aee8fe2dd3f91958c7df
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 16:10:01 +0200

    Aj Sakakil shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Ad elta stelpur"))
+			if (fileEntry.getName().contains("Aj Sakakil"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
