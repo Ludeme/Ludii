72a10f6643ac8744653452d4359df3d3fea8dcb5
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:23:32 +0200

    Chukaray shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Chaturaji"))
+			if (fileEntry.getName().contains("Chukaray"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
