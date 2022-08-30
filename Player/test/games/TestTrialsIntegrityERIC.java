bd1fdeb69de7bfdc456fb65998d20f0478141649
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:15:18 +0200

    Boolik shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Bheri Bakhri"))
+			if (fileEntry.getName().contains("Boolik"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
