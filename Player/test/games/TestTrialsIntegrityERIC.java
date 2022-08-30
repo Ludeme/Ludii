daf9cb9dfb625b85818aaf171b5560e89ba69dc7
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:34:14 +0200

    Deleb shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Daldosa"))
+			if (fileEntry.getName().contains("Deleb"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
