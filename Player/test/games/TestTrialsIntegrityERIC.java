4bed428bc512801ebfd196d3daad4d9b358adfb2
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 10:43:46 +0200

    minor

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Stratomic"))
+			if (fileEntry.getName().contains("Symmetric Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
