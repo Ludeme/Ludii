5ad74dc2c9fc1ddde3857add145a147f7dc8c8db
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 16:30:00 +0200

    Aj Sayil shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Aj Sayil"))
+			if (fileEntry.getName().contains("Aj Sina'anil"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
