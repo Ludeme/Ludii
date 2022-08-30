442d9688414caf12197db44f67dedfb0fbea77e4
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:20:27 +0200

    Chaturaji a bit shorter

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Boxijn Barildaan"))
+			if (fileEntry.getName().contains("Chaturaji"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
