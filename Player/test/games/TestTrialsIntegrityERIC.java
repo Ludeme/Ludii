1ed785fa572fffb78f940b42a06793834ecef035
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 14:08:00 +0200

    Yonin Shogi shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Unashogi"))
+			if (fileEntry.getName().contains("Yonin Shogi"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
