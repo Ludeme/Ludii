26ad569664a3e10a61844eb1fce9830ed3ad9cb3
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:36:26 +0200

    Fang shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Deleb"))
+			if (fileEntry.getName().contains("Fang"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
