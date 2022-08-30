80554cbf2bdcad59d1de68786edc277bb1afae2c
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 12:16:04 +0200

    Ultimate Chess shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Symmetric Chess"))
+			if (fileEntry.getName().contains("Ultimate Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
