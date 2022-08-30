c98467afede7d4191150445c9d16628515b5b46b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:17:09 +0200

    Boxijn Barildaan shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Boolik"))
+			if (fileEntry.getName().contains("Boxijn Barildaan"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
