40fd21bb1761ae37db9139df2971c49478f24ded
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 17:26:00 +0200

    Daldos shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Crusade"))
+			if (fileEntry.getName().contains("Daldos"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
