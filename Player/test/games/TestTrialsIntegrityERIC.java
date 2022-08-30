f031d575334844c83c8cd84ec17b49b6a3382f10
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-30 16:39:49 +0200

    A K'aak'il shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Aj T'iwil"))
+			if (fileEntry.getName().contains("A K'aak'il"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
