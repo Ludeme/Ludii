ffc828638298a78650b12935956311eaab519317
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 10:13:35 +0200

    Kioz shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Ja-Jeon-Geo-Gonu"))
+			if (fileEntry.getName().contains("Kioz"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
