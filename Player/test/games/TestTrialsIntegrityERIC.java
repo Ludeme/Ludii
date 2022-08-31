62e80c78049b09d456acd334c86f5ce8956a7317
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 10:43:41 +0200

    Puluc shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Oust"))
+			if (fileEntry.getName().contains("Puluc"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
