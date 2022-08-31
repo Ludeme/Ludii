ab3edbffd12c1d792d33708bc1b7852bce2cf8ac
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 13:44:33 +0200

    Tank Tactics shroter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tablas Astronomias"))
+			if (fileEntry.getName().contains("Tank Tactics"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
