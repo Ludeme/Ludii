87051b72a0f44adf66d14163f876f7cd8691c39a
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 14:00:58 +0200

    Elea and Mono have a swap rule acc to Michael

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Zola"))
+			if (fileEntry.getName().contains("Elea"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
