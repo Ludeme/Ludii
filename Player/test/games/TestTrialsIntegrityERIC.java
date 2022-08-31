d1495950372df62a9e3b8bee002b4053b1bd6107
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 10:18:01 +0200

    Kiust Oyun shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Kioz"))
+			if (fileEntry.getName().contains("Kiust Oyun"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
