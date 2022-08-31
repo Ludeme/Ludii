057f4b131adcef55707552720e5668dee2c4fa73
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 10:54:04 +0200

    Sahkku shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Rampart"))
+			if (fileEntry.getName().contains("Sahkku"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
