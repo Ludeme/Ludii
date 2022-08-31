9b01a448a499184550a12b3ae5b7ff786fdab58b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 13:45:35 +0200

    Toono shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Tank Tactics"))
+			if (fileEntry.getName().contains("Toono"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
