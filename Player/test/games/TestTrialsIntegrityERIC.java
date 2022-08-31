87d5de3e11ba2cd819ed2c5e9f1ad191cd8d50bb
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 10:45:54 +0200

    Rampart shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Puluc"))
+			if (fileEntry.getName().contains("Rampart"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
