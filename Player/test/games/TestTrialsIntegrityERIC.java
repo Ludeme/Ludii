138f5e21c9d1cc7558ff10627a0e03e6883fe620
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 17:11:26 +0200

    all xp games shorter descs

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Press Ups"))
+			if (fileEntry.getName().contains("Netted"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
