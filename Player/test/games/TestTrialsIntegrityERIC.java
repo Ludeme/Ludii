864f64d83cd068a70be5be478a78c84b15df8b18
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 14:12:26 +0200

    Cannon shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Elea"))
+			if (fileEntry.getName().contains("Cannon"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
