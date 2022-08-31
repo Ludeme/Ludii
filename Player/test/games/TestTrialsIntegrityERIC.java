e3b25281c3e91e6564d1895eb7c09dea09ad9263
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 11:58:32 +0200

    Skirmish (GDL) shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Siryu (War)"))
+			if (fileEntry.getName().contains("Skirmish (GDL)"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
