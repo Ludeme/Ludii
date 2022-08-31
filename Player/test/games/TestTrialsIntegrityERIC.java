2351b417adf1140407706dfb23bf62a6e9bce8d3
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 11:13:08 +0200

    Sijat El Taba shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("Sig (Western Sahara)"))
+			if (fileEntry.getName().contains("Sijat El Taba"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
