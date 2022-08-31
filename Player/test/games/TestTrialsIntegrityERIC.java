4e61b20b71f1c6453c9f4d724f2fa7e0ae1384e7
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-08-31 14:43:10 +0200

    Kriegsspiel shorter desc

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("King And Courtesan"))
+			if (fileEntry.getName().contains("Kriegsspiel"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
