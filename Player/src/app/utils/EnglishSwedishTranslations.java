d177f3a2d731565f652651670970efa141abc344
Matthew Stephenson <matthew.stephenson@maastrichtuniversity.nl>
2022-09-12 17:15:04 +0200

    english swedish updated

--- Player/src/app/utils/EnglishSwedishTranslations.java
+++ Player/src/app/utils/EnglishSwedishTranslations.java
@@ -3,40 +3,40 @@ package app.utils;
 public enum EnglishSwedishTranslations 
 {
 	// Menu Text
-	MYOGTITLE("Make Your Own Game", "TODO"),
-	CHOOSEBOARD("Choose a board", "TODO"),
-	DRAGPIECES("Drag pieces onto board", "TODO"),
-	MOVEMENT("Movement", "TODO"),
-	CAPTURE("Capture", "TODO"),
-	CHOOSEGOALS("Choose goal(s)", "TODO"),
+	MYOGTITLE("Make Your Own Game", "TODO SWEDISH"),
+	CHOOSEBOARD("Choose a board", "TODO SWEDISH"),
+	DRAGPIECES("Drag pieces onto board", "TODO SWEDISH"),
+	MOVEMENT("Movement", "TODO SWEDISH"),
+	CAPTURE("Capture", "TODO SWEDISH"),
+	CHOOSEGOALS("Choose goal(s)", "TODO SWEDISH"),
 	
 	// Button Text
-	RESET("Reset", "TODO"),
-	PLAY("Play", "TODO"),
-	PLAYAGAIN("Player Again", "TODO"),
-	EDIT("Edit", "TODO"),
-	HUMANVSHUMAN("Human vs Human", "TODO"),
-	HUMANVSAI("Huma vs AI", "TODO"),
+	RESET("Reset", "TODO SWEDISH"),
+	PLAY("Play", "TODO SWEDISH"),
+	PLAYAGAIN("Player Again", "TODO SWEDISH"),
+	EDIT("Edit", "TODO SWEDISH"),
+	HUMANVSHUMAN("Human vs Human", "TODO SWEDISH"),
+	HUMANVSAI("Huma vs AI", "TODO SWEDISH"),
 	
 	// Goal Options
-	LINE3("Line 3", "TODO"),
-	LINE4("Line 4", "TODO"),
-	ELIMINATE("Eliminate", "TODO"),
-	BLOCK("Block", "TODO"),
-	SURROUND("Surround", "TODO"),
+	LINE3("Line 3", "TODO SWEDISH"),
+	LINE4("Line 4", "TODO SWEDISH"),
+	ELIMINATE("Eliminate", "TODO SWEDISH"),
+	BLOCK("Block", "TODO SWEDISH"),
+	SURROUND("Surround", "TODO SWEDISH"),
 	
 	// Move Options
-	STEP("Step", "TODO"),
-	SLIDE("Slide", "TODO"),
-	KNIGHT("Knight", "TODO"),
-	ADD("Add", "TODO"),
-	ANY("Any", "TODO"),
+	STEP("Step", "TODO SWEDISH"),
+	SLIDE("Slide", "TODO SWEDISH"),
+	KNIGHT("Knight", "TODO SWEDISH"),
+	ADD("Add", "TODO SWEDISH"),
+	ANY("Any", "TODO SWEDISH"),
 	
 	// Capture Options
-	REPLACE("Replace", "TODO"),
-	HOP("Hop", "TODO"),
-	FLANK("Flank", "TODO"),
-	NEIGHBOR("Neighbor", "TODO"),
+	REPLACE("Replace", "TODO SWEDISH"),
+	HOP("Hop", "TODO SWEDISH"),
+	FLANK("Flank", "TODO SWEDISH"),
+	NEIGHBOR("Neighbor", "TODO SWEDISH"),
 	;
 
 	//-------------------------------------------------------------------------
