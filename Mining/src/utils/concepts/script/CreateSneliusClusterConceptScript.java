1c48f38910ee1bc3ef059a4ccc5fc5ac4e10fe37
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-09-12 11:31:41 +0200

    scripts updated for museum xp

--- Mining/src/utils/concepts/script/CreateSneliusClusterConceptScript.java
+++ Mining/src/utils/concepts/script/CreateSneliusClusterConceptScript.java
@@ -25,9 +25,9 @@ public class CreateSneliusClusterConceptScript
 	{
 		final int numPlayout = 100;
 		final int maxTime = 175000;
-		final int maxMove = 5000; //250; //5000; // Constants.DEFAULT_MOVES_LIMIT;
+		final int maxMove = 250; //250; //5000; // Constants.DEFAULT_MOVES_LIMIT;
 		final int thinkingTime = 1;
-		final String agentName = "Alpha-Beta"; // Can be "UCT",  "Alpha-Beta", "Alpha-Beta-UCT", "AB-Odd-Even", "ABONEPLY", "UCTONEPLY", or "Random"
+		final String agentName = "Tree_5_vs_TSPG"; //"Alpha-Beta"; // Can be "UCT",  "Alpha-Beta", "Alpha-Beta-UCT", "AB-Odd-Even", "ABONEPLY", "UCTONEPLY", or "Random"
 		final String folder = "/../Trials/Trials"+agentName; //""; //"/../Trials/TrialsAll";
 		final String mainScriptName = "StateConcepts.sh";
 		final String folderName = "Concepts"+agentName;
@@ -59,6 +59,10 @@ public class CreateSneliusClusterConceptScript
 				if (gameName.replaceAll(Pattern.quote("\\"), "/").contains("reconstruction"))
 					continue;
 
+				// For the museum game.
+//				if(!gameName.contains("Thermenmuseum"))
+//					continue;
+				
 				final Game game = GameLoader.loadGameFromName(gameName);
 				
 //				final String fileName = gameName.isEmpty() ? ""
