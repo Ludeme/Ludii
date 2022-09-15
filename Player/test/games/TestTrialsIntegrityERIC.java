7230beec025c919bdf3d62052b2bd01816f60062
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-09-15 09:33:38 +0200

    minor

--- Player/test/games/TestTrialsIntegrityERIC.java
+++ Player/test/games/TestTrialsIntegrityERIC.java
@@ -116,7 +116,7 @@ public class TestTrialsIntegrityERIC
 
 		for (final File fileEntry : entries)
 		{
-			if (fileEntry.getName().contains("58 Holes"))
+			if (fileEntry.getName().contains("Nodal Chess"))
 			{
 				if (fileEntry.getName().contains(gameToReached) || gameToReached.length() == 0)
 					gameReached = true;
