17ffad5ce1d2a790bea5448fd2e2f5e4a3e3a5b8
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-10-07 16:08:19 +0200

    teh last sow

--- Player/test/travis/recons/ReconstructionTest.java
+++ Player/test/travis/recons/ReconstructionTest.java
@@ -42,7 +42,7 @@ public class ReconstructionTest
 		{
 			//if (!fileName.replaceAll(Pattern.quote("\\"), "/").contains("/lud/test/eric/recons/"))
 			//if (!fileName.replaceAll(Pattern.quote("\\"), "/").contains("/lud/reconstruction/"))
-			if (!fileName.replaceAll(Pattern.quote("\\"), "/").contains("/lud/reconstruction/board/sow/other/Tami"))
+			if (!fileName.replaceAll(Pattern.quote("\\"), "/").contains("/lud/reconstruction/board/sow/two_rows/Sai"))
 				continue;
 			
 			// Get game description from resource
