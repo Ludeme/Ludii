e99a2175c10ac721780e2fe34dcca1870fc910e1
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-10-07 16:15:15 +0200

    lines too

--- Player/test/travis/recons/ReconstructionTest.java
+++ Player/test/travis/recons/ReconstructionTest.java
@@ -42,7 +42,7 @@ public class ReconstructionTest
 		{
 			//if (!fileName.replaceAll(Pattern.quote("\\"), "/").contains("/lud/test/eric/recons/"))
 			//if (!fileName.replaceAll(Pattern.quote("\\"), "/").contains("/lud/reconstruction/"))
-			if (!fileName.replaceAll(Pattern.quote("\\"), "/").contains("/lud/reconstruction/board/sow/two_rows/Sai"))
+			if (!fileName.replaceAll(Pattern.quote("\\"), "/").contains("/lud/reconstruction/board/space/line/Rwandan Alignment Game"))
 				continue;
 			
 			// Get game description from resource
