80bc9f0a6a552ffa0e79cd7107b3761910df8cc1
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-10-27 12:36:03 +0200

    fixed

--- Player/src/app/utils/QrCodeGeneration.java
+++ Player/src/app/utils/QrCodeGeneration.java
@@ -3,9 +3,6 @@ package app.utils;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
 
