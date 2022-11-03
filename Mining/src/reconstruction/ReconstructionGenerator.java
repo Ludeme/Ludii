d67572cf0ef12e497a728cd2746680abde0f340c
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-03 13:13:42 +0100

    utils package for reconstruction

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -23,6 +23,7 @@ import main.grammar.Description;
 import other.GameLoader;
 import other.concept.Concept;
 import reconstruction.completer.CompleterWithPrepro;
+import reconstruction.utils.FormatReconstructionOutputs;
 
 /**
  * Reconstruction Generator.
@@ -153,7 +154,7 @@ public class ReconstructionGenerator
 					for (int n = 0; n < completions.size(); n++) 
 					{
 						final Completion completion = completions.get(n);
-						final String completionRaw = indentNicely(StringRoutines.unformatOneLineDesc(completion.raw()));
+						final String completionRaw = FormatReconstructionOutputs.indentNicely(StringRoutines.unformatOneLineDesc(completion.raw()));
 						
 						// Test if the completion compiles.
 						Game game = null;
@@ -230,89 +231,5 @@ public class ReconstructionGenerator
 		final long stopAt = System.nanoTime();
 		final double secs = (stopAt - startAt) / 1000000000.0;
 		System.out.println("\nDone in " + secs + "s.");
-	}
-
-	//-----------------------------------------------------------------------------
-
-	/**
-	 * Nicely indents the description in entry.
-	 */
-	public static String indentNicely(final String desc)
-	{
-		final String linesArray[] = desc.split("\\r?\\n");
-		final List<String> lines = new ArrayList<String>();
-        for (int n = 0; n < linesArray.length; n++)
-        	lines.add(linesArray[n]);
-		          
-        // Left justify all lines
-        for (int n = 0; n < lines.size(); n++)
-        {
-        	String str = lines.get(n);
-        	final int c = 0;
-        	while (c < str.length() && (str.charAt(c) == ' ' || str.charAt(c) == '\t'))
-        		str = str.substring(1);
-        	
-        	lines.remove(n);
-        	lines.add(n, str);
-        }
-
-        removeDoubleEmptyLines(lines);
-        indentLines(lines);
-        
-        final StringBuffer outputDesc = new StringBuffer();
-        for (final String result : lines)
-        	outputDesc.append(result + "\n");
-        
-        return outputDesc.toString();
-    }
-	
-	/**
-	 * Removes double empty lines.
-	 */
-	final static void removeDoubleEmptyLines(final List<String> lines)
-	{
-        int n = 1;
-        while (n < lines.size())
-        {
-        	if (lines.get(n).equals("") && lines.get(n-1).equals(""))
-        		lines.remove(n);
-        	else
-        		n++;
-        }
- 	}
-	
-	/**
-	 * Nicely indents the lines of a desc.
-	 */
-	final static void indentLines(final List<String> lines)
-	{
-		final String indentString = "    ";
-        int indent = 0;
-        for (int n = 0; n < lines.size(); n++)
-        {
-        	String str = lines.get(n);
-        	
-        	final int numOpen  = StringRoutines.numChar(str, '(');  // don't count curly braces!
-        	final int numClose = StringRoutines.numChar(str, ')');
-        	
-        	final int difference = numOpen - numClose;
-        	
-        	if (difference < 0)
-        	{
-        		// Unindent from this line
-        		indent += difference;
-        		if (indent < 0)
-        			indent = 0;
-        	}
-        	        	
-        	for (int step = 0; step < indent; step++)
-        		str = indentString + str; 
- 
-   	       	lines.remove(n);
-        	lines.add(n, str);
-        	
-        	if (difference > 0)
-        		indent += difference;  // indent from next line
-        }
 	}
 }
