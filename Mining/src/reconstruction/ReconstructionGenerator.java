8d0f930cdbe555de66df9a31321543f086a8f8dd
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-18 13:49:59 +0100

    csv updated for recons

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -40,8 +40,8 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/race/escape/Barail";
-	//final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/replacement/checkmate/chaturanga/Samantsy";
+	//final static String defaultReconsPath        = "/lud/reconstruction/pending/board/race/escape/Barail";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/replacement/checkmate/chaturanga/Samantsy";
 	//final static String defaultReconsPath        = "/lud/reconstruction/board/race/other/Coptic Game";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Hnefatafl";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Senet";
