945bc375d7da1d2e57be7f2610633ca545860953
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-19 23:02:35 +0100

    Adzua reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Adzua (Children)";
+	final static String gameName        = "Adzua";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Adzua (Children)";
+	final static String gameName             = "Adzua";
 	
 	public static void main(final String[] args)
 	{
