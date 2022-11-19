6df4c7610fad525a82ed824cb93017dff0c500de
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-19 15:51:31 +0100

    Pasakakrida (Type 5) reconstruted

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Pasakakrida (Type 4)";
+	final static String gameName        = "Pasakakrida (Type 5)";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
