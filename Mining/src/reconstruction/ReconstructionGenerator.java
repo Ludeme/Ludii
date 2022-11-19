fc56088bfda782b60d44f1213b453ed9ffc1f0be
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-19 22:40:31 +0100

    Adzua (Children) reconstructed

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -41,7 +41,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/sow/two_rows/Owana";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/space/line/Adzua (Children)";
 	//final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Macheng";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Hnefatafl";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Senet";
