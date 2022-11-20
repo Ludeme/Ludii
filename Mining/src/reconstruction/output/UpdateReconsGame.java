1447ca50f6a3b0f74bdf942f8665ea0eaf99d452
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-20 21:34:56 +0100

    Damat reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Rongmei Naga Alignment Game";
+	final static String gameName             = "Damat";
 	
 	public static void main(final String[] args)
 	{
