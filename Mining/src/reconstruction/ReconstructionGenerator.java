8ba6f0d7eb736913ea4fbdf549842aa1afbda179
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-20 11:17:09 +0100

    Dimraw reconstructed

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -39,9 +39,9 @@ import utils.RandomAI;
 public class ReconstructionGenerator
 {
 	final static String defaultOutputPath        = "./res/recons/output/";
-	final static int    defaultNumReconsExpected = 13;
+	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/space/line/Davxar Zirge (Type 2)";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/space/line/Dimraw";
 	//final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Macheng";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Hnefatafl";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Senet";
--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Davxar Zirge (Type 2)";
+	final static String gameName        = "Dimraw";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
