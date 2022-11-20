c45101e173956db4d940b5b6e3b0e63c956bc3ab
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-20 21:09:28 +0100

    Rongmei Naga Alignment Game reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Mbenga Alignment Game";
+	final static String gameName             = "Rongmei Naga Alignment Game";
 	
 	public static void main(final String[] args)
 	{
