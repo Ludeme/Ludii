ea0d4f209773dbb24296f304a1e5ff2d6a7de172
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-20 22:14:50 +0100

    Yup'ik Checkers reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Damat";
+	final static String gameName             = "Yup'ik Checkers";
 	
 	public static void main(final String[] args)
 	{
