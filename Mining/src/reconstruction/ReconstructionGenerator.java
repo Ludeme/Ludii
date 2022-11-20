01c5d660e73f367f1029e53e2c3e66ce3032b038
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-20 10:38:56 +0100

    Davxar Zirge (Type 2) reconstructed

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -41,7 +41,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 13;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/space/line/Davxar Zirge (Type 1)";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/space/line/Davxar Zirge (Type 2)";
 	//final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Macheng";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Hnefatafl";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Senet";
--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Davxar Zirge (Type 1)";
+	final static String gameName             = "Davxar Zirge (Type 2)";
 	
 	public static void main(final String[] args)
 	{
