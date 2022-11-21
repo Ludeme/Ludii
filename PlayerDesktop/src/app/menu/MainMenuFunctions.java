ef17a22f1a00278494b2de22d43d6a3bc18906cb
Matthew Stephenson <matthew.stephenson@maastrichtuniversity.nl>
2022-11-21 11:57:07 +0100

    message for Eric

--- PlayerDesktop/src/app/menu/MainMenuFunctions.java
+++ PlayerDesktop/src/app/menu/MainMenuFunctions.java
@@ -278,6 +278,7 @@ public class MainMenuFunctions extends JMenuBar
 			    {
 			    	if (gameChoice >= gameOptions.size()) 
 			    	{
+			    		System.out.println("Thumbnails Done.");
 			            t.cancel();
 			            t.purge();
 			            return;
