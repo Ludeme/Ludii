d6ad503e6d95449df1f55df87c8341bbfd7debc3
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-21 15:00:39 +0100

    Ufuba wa Hulana reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Rongmei Naga Capturing Game";
+	final static String gameName             = "Ufuba wa Hulana";
 	
 	public static void main(final String[] args)
 	{
