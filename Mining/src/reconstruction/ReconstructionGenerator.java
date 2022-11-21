1e7e3b6b3ecb99a9309a054eab21ab421d78d896
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-21 12:44:04 +0100

    Smasandyutakankarikrida reconstructed

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -39,9 +39,9 @@ import utils.RandomAI;
 public class ReconstructionGenerator
 {
 	final static String defaultOutputPath        = "./res/recons/output/";
-	final static int    defaultNumReconsExpected = 5;
+	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/leaping/lines/Manipur Capturing Game";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/leaping/lines/Smasandyutakankarikrida";
 	//final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Macheng";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Hnefatafl";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Senet";
