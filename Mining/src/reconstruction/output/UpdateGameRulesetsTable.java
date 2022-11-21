78c14a0ee22139304a5a6c1a76975c135529b3fa
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-21 22:04:58 +0100

    Wan Fu Cave Game reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Riga";
+	final static String gameName        = "Wan Fu Cave Game";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
