7897d7c36f329260df7ef0cbe70bb115d51e588f
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-21 16:33:38 +0100

    Skak reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Shatranj al-Kabir (Constantinople)";
+	final static String gameName        = "Skak";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Shatranj al-Kabir (Constantinople)";
+	final static String gameName             = "Skak";
 	
 	public static void main(final String[] args)
 	{
