97f443451c16959bac7854362e040dabec04d26d
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-22 20:11:39 +0100

    Nardshir reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Cumi";
+	final static String gameName        = "Nardshir";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
