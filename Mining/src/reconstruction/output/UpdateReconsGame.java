14495f0eb318da1e62db02c054375bf5d5b05884
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-24 17:40:00 +0100

    Seven-Fang reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Cheng Fang Cheng Long";
+	final static String gameName             = "Seven-Fang";
 	
 	public static void main(final String[] args)
 	{
