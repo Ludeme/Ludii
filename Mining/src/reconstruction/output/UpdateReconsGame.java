1d91b73878126c07141409ea5a59954837a4af3b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-24 17:20:05 +0100

    Cheng Fang Cheng Long reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Four-Fang";
+	final static String gameName             = "Cheng Fang Cheng Long";
 	
 	public static void main(final String[] args)
 	{
