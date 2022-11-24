6014ec09767ec8fa403de21b9eb82e4b8517ebfd
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-24 18:03:26 +0100

    Eleven-Fang reconstructed

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -42,7 +42,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Seven-Fang";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Eleven-Fang";
 	//final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Macheng";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Hnefatafl";
 	//final static String defaultReconsPath        = "/lud/test/eric/recons/Senet";
