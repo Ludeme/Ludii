0d79f3f16e2f6dd57a368c091249f1bbbf93c2d0
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-26 21:10:25 +0100

    Diris reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Li'b al-Sidr";
+	final static String gameName             = "Diris";
 	
 	public static void main(final String[] args)
 	{
