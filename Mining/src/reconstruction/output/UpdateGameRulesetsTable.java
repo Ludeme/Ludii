034f0b21fa49614928846d433320db2aa586e876
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-26 21:46:56 +0100

    Che lo reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Diris";
+	final static String gameName        = "Che Lo";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
