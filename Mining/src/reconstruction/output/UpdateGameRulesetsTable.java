e88641b6e31bcfc282555ba3b1f91856510a453e
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-11-26 17:12:38 +0100

    Roman Small Merels reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Sgjough";
+	final static String gameName        = "Roman Small Merels";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Sgjough";
+	final static String gameName             = "Roman Small Merels";
 	
 	public static void main(final String[] args)
 	{
