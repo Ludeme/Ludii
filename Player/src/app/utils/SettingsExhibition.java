be44caab7b2c9ea2061bcebcf2813f6cd9bb3870
Matthew Stephenson <matthew.stephenson@maastrichtuniversity.nl>
2022-12-05 15:51:14 +0100

    instructions

--- Player/src/app/utils/SettingsExhibition.java
+++ Player/src/app/utils/SettingsExhibition.java
@@ -10,10 +10,13 @@ public class SettingsExhibition
 	/** If the app should be loaded in the exhibition display format. */
 	public static final boolean exhibitionVersion = false;
 	
+	/** The resolution of the app (some aspects may be hard-coded to this size). */
 	public static final int exhibitionDisplayWidth = 1920;
 	public static final int exhibitionDisplayHeight = 1080;
 	
+	/** The game to load (there exists both an english and swedish version of each game. */
 	public static final String exhibitionGamePath = "/lud/wip/exhibition/Senet Exhibition Swedish.lud";
 	
+	/** If Player 2 should be controlled by an AI agent. */
 	public static final boolean againstAI = false;
 }
