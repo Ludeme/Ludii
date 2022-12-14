1ccf41361f6fa1d2c35905ec3a91e24284c8f8c4
Matthew Stephenson <matthew.stephenson@maastrichtuniversity.nl>
2022-12-14 08:23:01 +0100

    in depth updates

--- Player/src/app/utils/SettingsExhibition.java
+++ Player/src/app/utils/SettingsExhibition.java
@@ -9,16 +9,42 @@ public class SettingsExhibition
 {
 	/** If the app should be loaded in the exhibition display 
 	 * format. */
-	public static final boolean exhibitionVersion = false;
+	public static final boolean exhibitionVersion = true;
 	
 	/** The resolution of the app (some aspects may be hard-coded to this size). */
 	public static final int exhibitionDisplayWidth = 1920;
 	public static final int exhibitionDisplayHeight = 1080;
 	
 	/** The game to load (there exists both an English and Swedish version of each game). */
-	public static final String exhibitionGamePath = "/lud/wip/exhibition/Sahkku Exhibition English.lud";
+	public static final String exhibitionGamePath = "/lud/wip/exhibition/Baghchal Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Hnefatafl Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Mu Torere Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Mweso Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Nard Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Papan Dakon Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Sahkku Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Senet Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Shatranj Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Shodra Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Toguz Kumalak Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Wari Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Weiqi Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Xiangqi Exhibition English.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Hnefatafl Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Mu Torere Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Mweso Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Nard Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Papan Dakon Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Sahkku Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Senet Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Shatranj Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Shodra Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Toguz Kumalak Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Wari Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Weiqi Exhibition Swedish.lud";
+//	public static final String exhibitionGamePath = "/lud/wip/exhibition/Xiangqi Exhibition Swedish.lud";
 	
 	/** If Player 2 should be controlled by an AI agent. */
 	public static final boolean againstAI = true;
-	public static final double thinkingTime = 3.0;
+	public static final double thinkingTime = 2.0;
 }
