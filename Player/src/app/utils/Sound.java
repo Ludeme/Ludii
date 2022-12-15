88d85fad2dd0daef86f0cba3e16e7323f09e5f41
Matthew Stephenson <matthew.stephenson@maastrichtuniversity.nl>
2022-12-15 08:42:22 +0100

    fixed sound for jar build

--- Player/src/app/utils/Sound.java
+++ Player/src/app/utils/Sound.java
@@ -1,11 +1,13 @@
 package app.utils;
 
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 
 /**
  * Functions relating to the playing of sounds/music.
@@ -17,44 +19,13 @@ public class Sound
 	
 	//-------------------------------------------------------------------------
 
-	/**
-	 * Play a specific sound once.
-	 */
-	public static synchronized void playSound(final String soundName) 
-	{
-		// TODO need to update this to work on the .jar build.
-		final String soundPath = "../Common/res/audio/" + soundName + ".wav";
-		
-		new Thread(new Runnable() 
-		{
-			// Audio playing doesn't work with standard try-with-resource approach.
-			// https://stackoverflow.com/questions/25564980/java-use-a-clip-and-a-try-with-resources-block-which-results-with-no-sound
-			@SuppressWarnings("resource")
-			@Override
-			public void run() 
-		    {
-				try 
-				{
-					final AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(soundPath));
-			        final DataLine.Info info = new DataLine.Info(Clip.class, inputStream.getFormat());
-			        final Clip clip = (Clip)AudioSystem.getLine(info);
-			        clip.open(inputStream);
-					clip.start(); 
-				} 
-				catch (final Exception e) 
-				{
-					e.printStackTrace();
-				}
-		    }
-		}).start();
-	}
-	
 //	/**
 //	 * Play a specific sound once.
 //	 */
 //	public static synchronized void playSound(final String soundName) 
 //	{
-//		final String soundPath = "/" + soundName + ".wav";
+//		final String soundPath = "../Common/res/audio/" + soundName + ".wav";
+//		
 //		new Thread(new Runnable() 
 //		{
 //			// Audio playing doesn't work with standard try-with-resource approach.
@@ -65,31 +36,11 @@ public class Sound
 //		    {
 //				try 
 //				{
-//					final Clip clip = AudioSystem.getClip();
-//					final InputStream is = DesktopApp.class.getResourceAsStream(soundPath);
-//					final BufferedInputStream bufferedIS = new BufferedInputStream(is);
-//					final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedIS);	
-//					clip.open(audioInputStream);
-//					
-////					FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-////					float range = volume.getMaximum() - volume.getMinimum();
-////					float gain = (range * 0.5f) + volume.getMinimum();
-////					volume.setValue(gain);
-//					
+//					final AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(soundPath));
+//			        final DataLine.Info info = new DataLine.Info(Clip.class, inputStream.getFormat());
+//			        final Clip clip = (Clip)AudioSystem.getLine(info);
+//			        clip.open(inputStream);
 //					clip.start(); 
-//					bufferedIS.close();
-//					audioInputStream.close();
-//					
-//					// Close the clip only once it has finished playing.
-//					clip.addLineListener(new LineListener() 
-//					{
-//					    @Override
-//					    public void update(final LineEvent event) 
-//					    {
-//					        if (event.getType() == LineEvent.Type.STOP)
-//					            clip.close();
-//					    }
-//					});
 //				} 
 //				catch (final Exception e) 
 //				{
@@ -98,5 +49,50 @@ public class Sound
 //		    }
 //		}).start();
 //	}
+	
+	/**
+	 * Play a specific sound once.
+	 */
+	public static synchronized void playSound(final String soundName) 
+	{
+		final String soundPath = "/" + soundName + ".wav";
+		new Thread(new Runnable() 
+		{
+			// Audio playing doesn't work with standard try-with-resource approach.
+			// https://stackoverflow.com/questions/25564980/java-use-a-clip-and-a-try-with-resources-block-which-results-with-no-sound
+			@SuppressWarnings("resource")
+			@Override
+			public void run() 
+		    {
+				try 
+				{
+					final Clip clip = AudioSystem.getClip();
+					final InputStream is = Sound.class.getResourceAsStream(soundPath);
+					final BufferedInputStream bufferedIS = new BufferedInputStream(is);
+					final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedIS);	
+					clip.open(audioInputStream);
+					
+					clip.start(); 
+					bufferedIS.close();
+					audioInputStream.close();
+					
+					// Close the clip only once it has finished playing.
+					clip.addLineListener(new LineListener() 
+					{
+					    @Override
+					    public void update(final LineEvent event) 
+					    {
+					        if (event.getType() == LineEvent.Type.STOP)
+					            clip.close();
+					    }
+					});
+				} 
+				catch (final Exception e) 
+				{
+					e.printStackTrace();
+				}
+		    }
+		}).start();
+	}
 	
 }
