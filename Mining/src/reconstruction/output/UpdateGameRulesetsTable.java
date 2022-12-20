0a42f96f5c6a28c66c33c90b4784b38ffd741e0e
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-20 22:53:43 +0100

    Aiyawatstani recosn geo

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Adzua";
+	final static String gameName        = "Aiyawatstani";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Adzua";
+	final static String gameName             = "Aiyawatstani";
 	
 	public static void main(final String[] args)
 	{
