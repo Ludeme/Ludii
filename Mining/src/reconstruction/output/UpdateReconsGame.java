dac891c8e074a33abf6ce3066fbab7a44a2856af
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-20 20:30:17 +0100

    Talyn Buga recons with geo

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Ikh Buga";
+	final static String gameName             = "Talyn Buga";
 	
 	public static void main(final String[] args)
 	{
