6c4770ec8eabbeab45341639243d6d70a338326b
Eric.Piette <Eric.Piette@FSELAP0005.unimaas.nl>
2022-12-21 22:38:38 +0100

    for other laptop

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -40,9 +40,9 @@ import utils.RandomAI;
 public class ReconstructionGenerator
 {
 	final static String defaultOutputPath        = "./res/recons/output/";
-	final static int    defaultNumReconsExpected = 10;
+	final static int    defaultNumReconsExpected = 4;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/done/board/war/other/Seven-Fang";
+	final static String defaultReconsPath        = "/lud/reconstruction/done/board/war/other/Rongmei Naga Capturing Game";
 	//final static String defaultReconsPath        = "/lud/reconstruction/done/board/war/replacement/checkmate/chaturanga/Samantsy";
 	//final static String defaultReconsPath        = "/lud/reconstruction/validation/Canadian Draughts";
 	final static String defaultOptionName        = "Variant/Incomplete";
