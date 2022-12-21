46b800f1866a7bada001c3213e1d05f8f92a1a72
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-21 13:21:01 +0100

    Roman Small Merels reocns geo

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Rwandan Alignment Game";
+	final static String gameName             = "Roman Small Merels";
 	
 	public static void main(final String[] args)
 	{
