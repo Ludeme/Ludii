97d249e6d31551541caf8af3e982b0d76ac329c6
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-21 15:13:36 +0100

    Duqurjin recons geo

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Dris et-Tessa (Diagonals)";
+	final static String gameName             = "Duqurjin";
 	
 	public static void main(final String[] args)
 	{
