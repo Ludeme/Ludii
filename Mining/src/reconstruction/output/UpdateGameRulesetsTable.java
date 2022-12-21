a2adf9221bed895915f4f631aab4c87e2a4b3c91
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-21 15:22:36 +0100

    Hujura recons geo

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Cheng Fang Cheng Long";
+	final static String gameName        = "Hujura";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
