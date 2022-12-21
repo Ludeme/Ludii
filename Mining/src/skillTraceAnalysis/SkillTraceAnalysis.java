c267752bfaf944d723e4848e3823427d258dfbb9
Matthew Stephenson <matthew.stephenson@maastrichtuniversity.nl>
2022-12-21 12:35:11 +0100

    skill trace updated

--- Mining/src/skillTraceAnalysis/SkillTraceAnalysis.java
+++ Mining/src/skillTraceAnalysis/SkillTraceAnalysis.java
@@ -4,12 +4,18 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import game.Game;
 import main.FileHandling;
 import metrics.designer.SkillTrace;
 import other.GameLoader;
+import other.context.Context;
+import other.trial.Trial;
 
 /**
  * Calculates the Skill Trace statistics for all games in Ludii.
@@ -26,7 +32,28 @@ public class SkillTraceAnalysis
 	{		
 		final SkillTrace skillTraceMetric = new SkillTrace();
 		skillTraceMetric.setAddToDatabaseFile(true);
-		final String[] choices = FileHandling.listGames();
+		final String[] choicesUnsorted = FileHandling.listGames();
+		
+		// Order all games by their branching factor estimate.
+		final Map<String,Integer> choicesBranchingFactors = new HashMap<>();
+		for (final String s : choicesUnsorted)
+		{
+			if (FileHandling.shouldIgnoreLudRelease(s))
+				continue;
+			
+			// Load the game and estimate branching factor as the number of legal moves at the start of the game.
+			final String gameName = s.split("\\/")[s.split("\\/").length-1];
+			final Game game = GameLoader.loadGameFromName(gameName);
+			final Trial trial = new Trial(game);
+			final Context context = new Context(game, trial);
+			game.start(context);
+			final int bf = game.moves(context).count();
+			System.out.println(game.name() + " BF: " + bf);
+			choicesBranchingFactors.put(s, Integer.valueOf(bf));
+		}
+		final LinkedHashMap<String, Integer> choicesSortedBranchingFactors = new LinkedHashMap<>();
+		choicesBranchingFactors.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEachOrdered(x -> choicesSortedBranchingFactors.put(x.getKey(), x.getValue()));
+		final Set<String> choicesSorted = choicesSortedBranchingFactors.keySet();
 		
 		// Record games that have already been done, and should not be redone.
 		final List<String> gameNamesAlreadyDone = new ArrayList<>();
@@ -48,11 +75,8 @@ public class SkillTraceAnalysis
 			e.printStackTrace();
 		}
 		
-		for (final String s : choices)
+		for (final String s : choicesSorted)
 		{
-			if (FileHandling.shouldIgnoreLudRelease(s))
-				continue;
-			
 			final String gameName = s.split("\\/")[s.split("\\/").length-1];
 			final Game game = GameLoader.loadGameFromName(gameName);
 			if (gameNamesAlreadyDone.contains(game.name()))
