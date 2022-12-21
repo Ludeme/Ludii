843cf819d2b04eb04c716dae5091219b042c60cf
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-21 11:59:51 +0100

    auto indent

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Los Romanos Rencontrat";
+	final static String gameName        = "Lian Qi (Bohai)";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
