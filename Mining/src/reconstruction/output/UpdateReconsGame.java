5ccafa50ef6cab395b760d144b213d947a61c750
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-21 15:18:51 +0100

    Smasandyutakankarikrida recons geo

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Wan Fu Cave Game";
+	final static String gameName             = "Smasandyutakankarikrida";
 	
 	public static void main(final String[] args)
 	{
