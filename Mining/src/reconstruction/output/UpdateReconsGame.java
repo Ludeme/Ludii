432b940491c690207c7d168993f90020ddf71805
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-21 15:26:20 +0100

    Dris et-TEssa recons geo

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Shatranj al-Kabir (Constantinople)";
+	final static String gameName             = "Dris et-Tessa";
 	
 	public static void main(final String[] args)
 	{
