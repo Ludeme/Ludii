4df626247f2e14ce2f884055024e284e257a3de2
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-21 15:16:31 +0100

    Wan fu CAve Game recosn geo

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Duqurjin";
+	final static String gameName             = "Wan Fu Cave Game";
 	
 	public static void main(final String[] args)
 	{
