0c34ce222a71eea812d2d726a174e47a09de4028
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-21 15:41:02 +0100

    Eleven-Fang recons geo

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Mandoli";
+	final static String gameName        = "Eleven-Fang";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
