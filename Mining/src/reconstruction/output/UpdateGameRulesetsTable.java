7ddef2dd39d2ad92bac198c96036ce39f4dbce45
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-22 10:48:52 +0100

    Rongmei Naga Capturing Game recons geo

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Four-Fang";
+	final static String gameName        = "Rongmei Naga Capturing Game";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
