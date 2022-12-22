5238a295e25849a29e0d9d295b7c98e2c63bc4ae
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-22 14:18:38 +0100

    manipur capturing game recons geo

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Mbenga Alignment Game";
+	final static String gameName        = "Manipur Capturing Game";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
