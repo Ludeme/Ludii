5f0d8035c4c4817f562160d8cbbe42518921b9a3
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-23 22:32:51 +0100

    Guettal recons geo

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Pasakakrida (Type 6)";
+	final static String gameName        = "Guettal";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
