4f22022addcdbc5dbe2ffff51f571206e854b9a6
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-23 20:15:49 +0100

    type 4 recons geo

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Pasakakrida (Type 3)";
+	final static String gameName             = "Pasakakrida (Type 4)";
 	
 	public static void main(final String[] args)
 	{
