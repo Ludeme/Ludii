09c21c23cce3e472de824d6459329fc78189f28b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-23 18:33:23 +0100

    Type 2 recons geo

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Pasakakrida (Type 1)";
+	final static String gameName        = "Pasakakrida (Type 2)";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
