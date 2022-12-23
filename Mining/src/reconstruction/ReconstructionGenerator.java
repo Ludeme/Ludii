3b96834182f90f25b0593be334906d5989f972f7
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2022-12-23 20:46:55 +0100

    Type 5 recons geo

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -42,7 +42,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/done/board/race/escape/Pasakakrida (Type 4)";
+	final static String defaultReconsPath        = "/lud/reconstruction/done/board/race/escape/Pasakakrida (Type 5)";
 	//final static String defaultReconsPath        = "/lud/reconstruction/done/board/war/replacement/checkmate/chaturanga/Samantsy";
 	//final static String defaultReconsPath        = "/lud/reconstruction/validation/Canadian Draughts";
 	final static String defaultOptionName        = "Variant/Incomplete";
