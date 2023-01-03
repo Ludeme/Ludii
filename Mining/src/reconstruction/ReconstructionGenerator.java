6fa52dd3af19948e08763ef4805d81fb640127e0
Eric.Piette <Eric.Piette@FSELAP0005.unimaas.nl>
2023-01-03 10:44:56 +0100

    Bagh Bukree recons for other laptop

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -42,7 +42,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/done/board/war/leaping/lines/Guettal";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/hunt/Bagh Bukree";
 	//final static String defaultReconsPath        = "/lud/reconstruction/done/board/war/replacement/checkmate/chaturanga/Samantsy";
 	//final static String defaultReconsPath        = "/lud/reconstruction/validation/Canadian Draughts";
 	final static String defaultOptionName        = "Variant/Incomplete";
