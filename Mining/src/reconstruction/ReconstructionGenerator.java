42f5042e1d3efca842efe08a0b9189e9e65d86e0
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-05 14:22:21 +0100

    Cashlan Gherra redo. Because I forgot to add them in the db :(

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -42,7 +42,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 100000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/space/line/Picaria (Large)";
+	final static String defaultReconsPath        = "/lud/reconstruction/done/board/space/line/Cashlan Gherra";
 	//final static String defaultReconsPath        = "/lud/reconstruction/done/board/war/replacement/checkmate/chaturanga/Samantsy";
 	//final static String defaultReconsPath        = "/lud/reconstruction/validation/Canadian Draughts";
 	final static String defaultOptionName        = "Variant/Incomplete";
