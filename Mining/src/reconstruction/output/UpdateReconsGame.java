ed4cffe4250065dbce6c583fdd6ee58792a6281a
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-06 08:58:39 +0100

    Cashlan Gherra recons added to db and ludii

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Picaria (Large)";
+	final static String gameName             = "Cashlan Gherra";
 	
 	public static void main(final String[] args)
 	{
