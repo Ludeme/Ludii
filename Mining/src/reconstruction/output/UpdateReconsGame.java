2f112ada313bd234c80fdb2114ca28f5654e721e
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-11 14:17:35 +0100

    remove apit recons, they are incorrect

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Solitario";
+	final static String gameName             = "Apit";
 	
 	public static void main(final String[] args)
 	{
