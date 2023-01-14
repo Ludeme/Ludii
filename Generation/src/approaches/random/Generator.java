75d43e88416edd85bdaa8a2fde218632384bebd4
Alex Padula <alexpadula2001@gmail.com>
2023-01-14 16:15:03 +0100

    Update Generation/src/approaches/random/Generator.java
    Co-authored-by: Dennis Soemers <d.soemers@gmail.com>

--- Generation/src/approaches/random/Generator.java
+++ Generation/src/approaches/random/Generator.java
@@ -627,7 +627,7 @@ public class Generator
 				num = num % 4 + 1;
 			}
 			
-			str = str.substring(0, c) + num + str.substring(c + 5);
+			str = str.substring(0, c) + num + str.substring(c + "%dim%".length());
 		}
 						
 		return str;
