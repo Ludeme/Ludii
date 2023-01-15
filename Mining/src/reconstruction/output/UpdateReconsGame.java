5725b44b16ab74b62392789da8e520abef59f304
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-15 08:09:44 +0100

    Dame recons were half ok, too many chess, will run again

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Atlanbagj";
+	final static String gameName             = "Dame";
 	
 	public static void main(final String[] args)
 	{
