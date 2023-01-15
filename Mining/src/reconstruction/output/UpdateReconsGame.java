74dfb3619dd7d89d030bdaea1b42d22898f25cb1
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-15 12:36:01 +0100

    new try for it

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Dame";
+	final static String gameName             = "Ashanti Draughts";
 	
 	public static void main(final String[] args)
 	{
