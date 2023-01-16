f142c13344c86a6dde3427a5d2f4ac7995c9033e
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-16 11:59:35 +0100

    Smasandyutakankarikrida (Allahabad) reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Trique";
+	final static String gameName             = "Smasandyutakankarikrida (Allahabad)";
 	
 	public static void main(final String[] args)
 	{
