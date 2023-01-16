e2fa27bb95cdcac661a36648b4fde6dfcaabe981
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-16 12:08:11 +0100

    Lu Qi reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Smasandyutakankarikrida (Allahabad)";
+	final static String gameName        = "Lu Qi";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
