4d5aff3357b273b852685026826dbdeced645ddb
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-17 09:28:32 +0100

    Still not good for damdaman, let's try more constraints

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Lu Qi";
+	final static String gameName             = "Damdaman";
 	
 	public static void main(final String[] args)
 	{
