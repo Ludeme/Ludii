1f336bae5e271140be7f8812e1daebbdb4d7ffee
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-17 09:38:24 +0100

    Nin Adnai Kit Adnat reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Damdaman";
+	final static String gameName             = "Nin Adnai Kit Adnat";
 	
 	public static void main(final String[] args)
 	{
