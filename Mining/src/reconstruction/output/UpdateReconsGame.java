846e25788a37441dca3db430b9df53a3b073788b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-23 20:11:29 +0100

    Toto reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Shakhmaty (Early Modern)";
+	final static String gameName             = "Totolospi (War)";
 	
 	public static void main(final String[] args)
 	{
