3fdd0d0dae7f506d1b0cc71714db28ec24b0764b
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-23 08:31:01 +0100

    Shakhmaty (Medieval) ready to try some recons

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Chess (Siberia)";
+	final static String gameName        = "Shakhmaty (Early Modern)";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Chess (Siberia)";
+	final static String gameName             = "Shakhmaty (Early Modern)";
 	
 	public static void main(final String[] args)
 	{
