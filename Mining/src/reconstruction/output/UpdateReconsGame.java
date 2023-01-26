9e4489b929f0ed70449ccdaaf71a45e293cdd02d
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-26 10:31:32 +0100

    Shatranj Shir Bakri, some were good, but some not, so redo with removing the terrible ones

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Bazi Qamish";
+	final static String gameName             = "Shatranj Shir Bakri";
 	
 	public static void main(final String[] args)
 	{
