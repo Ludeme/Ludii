61997e6f9d8aff4e5a070e0b2fafcc1f1ee584e2
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-26 10:23:59 +0100

    csv updated for recons

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Shakhmaty (Early Modern)";
+	final static String gameName             = "Bazi Qamish";
 	
 	public static void main(final String[] args)
 	{
