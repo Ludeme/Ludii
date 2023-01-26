c8c8a30e78cc2f6283bc09bbde6c512ec2fb85a0
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-26 14:24:15 +0100

    Comina reconstructed, stuff from new jar + csv updated

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Shatranj al-Jawarhiya";
+	final static String gameName             = "Comina";
 	
 	public static void main(final String[] args)
 	{
