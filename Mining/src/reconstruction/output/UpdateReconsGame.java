7a4e53c3acfb56378f3ff195bc097cb26bf563f6
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-01-30 11:39:53 +0100

    Cumisitha reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Puma";
+	final static String gameName             = "Cumisitha";
 	
 	public static void main(final String[] args)
 	{
