7fc3ffb142e73aafc31de8e33793cbea57e05f5c
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-02-07 12:42:46 +0100

    Selayar Game reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Samoan Game";
+	final static String gameName             = "Selayar Game";
 	
 	public static void main(final String[] args)
 	{
