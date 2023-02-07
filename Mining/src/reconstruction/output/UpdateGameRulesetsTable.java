c456595f3948760a85be4efa314ddf32756f5256
Eric.Piette <Eric.Piette@FSELAP0256.unimaas.nl>
2023-02-07 11:57:05 +0100

    Tavlej reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Bakkis";
+	final static String gameName        = "Tavlej";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
