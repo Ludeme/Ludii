9a5977ee05b101611d284207294f976a2c56cc70
carni <carni@10.0.0.14>
2023-05-08 09:57:42 +0200

    Yi Reconstructed

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -42,7 +42,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 20000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/leaping/diagonal/Kokonag'n";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/space/territory/Yi";
 	final static String defaultOptionName        = "Variant/Incomplete";
 	
 	final static double  defaultConceptualWeight = 0.0;
