cb8731ed976fbf2d31e58c5f2b3340021d2aaa5a
carni <carni@10.0.0.14>
2023-05-08 15:32:12 +0200

    same for uct

--- Mining/src/utils/trials/CreateSneliusClusterTrialsScript.java
+++ Mining/src/utils/trials/CreateSneliusClusterTrialsScript.java
@@ -27,7 +27,7 @@ public class CreateSneliusClusterTrialsScript
 		final int maxMove = 5000; // Constants.DEFAULT_MOVES_LIMIT;
 		//final int allocatedMemoryJava = 4096;
 		final int thinkingTime = 1;
-		final String agentName = "Random"; // Can be "UCT",  "Alpha-Beta", "Alpha-Beta-UCT", "AB-Odd-Even", or "Random"
+		final String agentName = "UCT"; // Can be "UCT",  "Alpha-Beta", "Alpha-Beta-UCT", "AB-Odd-Even", or "Random"
 		final String clusterLogin = "cbrowne";
 		final String mainScriptName = "GenTrials.sh";
 		
