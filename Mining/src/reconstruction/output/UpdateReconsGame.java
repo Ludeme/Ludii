aed4608f69759a62db6d8e4ef35cd626ad429c2d
carni <carni@192.168.1.114>
2023-05-15 15:57:42 +0200

    files for release

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Kokonag'n";
+	final static String gameName             = "Shatranj (Egypt)";
 	
 	public static void main(final String[] args)
 	{
