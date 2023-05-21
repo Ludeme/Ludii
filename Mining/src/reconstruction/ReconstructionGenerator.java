21b77341927bc3168a259d27e4d0c728aece2a8b
carni <carni@Big-Laptop.home>
2023-05-21 11:49:55 +0200

    more to check tomorrow

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -42,7 +42,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 10;
 	final static int    defaultNumAttempts       = 20000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Andarraya";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Apit";
 	final static String defaultOptionName        = "Variant/Incomplete";
 	
 	final static double  defaultConceptualWeight = 0.0;
