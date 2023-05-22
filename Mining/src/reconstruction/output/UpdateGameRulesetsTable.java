2ea16c253e7d5f8d24d7da050750e792e13abefd
carni <carni@192.168.1.114>
2023-05-22 13:49:06 +0200

    Two-Player Chaturanga (al-Biruni) reconstructed

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Chatrang (Sassanian)";
+	final static String gameName        = "Two-Player Chaturanga (al-Biruni)";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
