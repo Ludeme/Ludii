d8c1dc36f2fdd0243f20fd2bc178f080663e845c
carni <carni@192.168.1.114>
2023-05-22 11:13:40 +0200

    Andarraya reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Apit";
+	final static String gameName             = "Andarraya";
 	
 	public static void main(final String[] args)
 	{
