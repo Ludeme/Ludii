a2d564fc92abe927b349842bbd82524f83d5cf26
carni <carni@192.168.1.114>
2023-05-24 13:35:43 +0200

    more to check

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Abanga";
+	final static String gameName        = "A-i-u";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
