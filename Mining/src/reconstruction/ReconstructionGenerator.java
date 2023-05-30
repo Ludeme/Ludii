33ee85259449342168ce59c4ce8dae35d302d5a5
carni <carni@10.0.0.14>
2023-05-30 14:22:21 +0200

    Ludus Rebellionis 1 reconstructed

--- Mining/src/reconstruction/ReconstructionGenerator.java
+++ Mining/src/reconstruction/ReconstructionGenerator.java
@@ -42,7 +42,7 @@ public class ReconstructionGenerator
 	final static String defaultOutputPath        = "./res/recons/output/";
 	final static int    defaultNumReconsExpected = 40;
 	final static int    defaultNumAttempts       = 20000;
-	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Ludus Rebellionis 2";
+	final static String defaultReconsPath        = "/lud/reconstruction/pending/board/war/other/Ludus Rebellionis 1";
 	final static String defaultOptionName        = "Variant/Incomplete";
 	
 	final static double  defaultConceptualWeight = 0.0;
