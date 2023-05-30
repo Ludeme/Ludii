141ebcf82000a258d1fd9aa5551e3fc3e9c66dd8
carni <carni@10.0.0.14>
2023-05-30 12:30:51 +0200

    same story for these two

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Ludus Rebellionis (Asymmetric)";
+	final static String gameName        = "Ludus Rebellionis 1";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
