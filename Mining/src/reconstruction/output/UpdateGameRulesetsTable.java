e2543fcf547d757375a3c4212d8ce963893f4a8e
carni <carni@10.0.0.14>
2023-05-30 10:59:10 +0200

    I did not like these ones, will gen more

--- Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
+++ Mining/src/reconstruction/output/UpdateGameRulesetsTable.java
@@ -34,7 +34,7 @@ public class UpdateGameRulesetsTable
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// The game name.
-	final static String gameName        = "Manddiare";
+	final static String gameName        = "Ludus Rebellionis (Asymmetric)";
 	
 	// The precision of the double to use.
 	final static int DOUBLE_PRECISION = 5;
--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Manddiare";
+	final static String gameName             = "Ludus Rebellionis (Asymmetric)";
 	
 	public static void main(final String[] args)
 	{
