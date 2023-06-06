a042894f0a9374bf3c01b3b6a9bec0fa67f9cfc9
carni <carni@Big-Laptop.home>
2023-06-06 09:31:08 +0200

    outch, bad recons

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Mak Khom";
+	final static String gameName             = "Kuang";
 	
 	public static void main(final String[] args)
 	{
