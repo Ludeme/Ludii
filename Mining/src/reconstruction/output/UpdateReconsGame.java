dd5a8ad01464ee1e97aacdb80dab0d82cb080d5b
carni <carni@Big-Laptop.home>
2023-06-07 13:01:17 +0200

    Haretavl (Horsens) reconstructed

--- Mining/src/reconstruction/output/UpdateReconsGame.java
+++ Mining/src/reconstruction/output/UpdateReconsGame.java
@@ -31,7 +31,7 @@ public class UpdateReconsGame
 	final static String pathReconstructed    = "./res/recons/output/";
 	
 	// game name.
-	final static String gameName             = "Kuang";
+	final static String gameName             = "Haretavl (Horsens)";
 	
 	public static void main(final String[] args)
 	{
