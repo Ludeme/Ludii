7c556701e2a1dfbf7d2aa7a8eeda599c1f28fe1e
Dennis Soemers <d.soemers@gmail.com>
2024-06-01 20:23:35 +0200

    more WIP better server experiments

--- Player/src/supplementary/experiments/eval/ParallelEvalMultiGamesMultiAgents.java
+++ Player/src/supplementary/experiments/eval/ParallelEvalMultiGamesMultiAgents.java
@@ -5,14 +5,37 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import features.feature_sets.network.JITSPatterNetFeatureSet;
+import game.Game;
+import gnu.trove.list.array.TIntArrayList;
 import main.CommandLineArgParse;
+import main.CommandLineArgParse.ArgOption;
+import main.CommandLineArgParse.OptionTypes;
+import main.DaemonThreadFactory;
+import main.collections.ListUtils;
+import other.AI;
+import other.GameLoader;
+import other.RankUtils;
+import other.context.Context;
+import other.model.Model;
+import other.trial.Trial;
+import utils.AIFactory;
+import utils.experiments.ResultsSummary;
 
 /**
  * Implementation of an experiment that evaluates multiple agents across multiple games.
@@ -26,7 +49,9 @@ public class ParallelEvalMultiGamesMultiAgents
 	
 	//-------------------------------------------------------------------------
 	
-	/* Auxiliary experiment setup */
+	protected int numCoresTotal;
+	protected int numThreadsPerTrial;
+	protected List<String> jsonFiles;
 
 	/** 
 	 * Whether to create a small GUI that can be used to manually interrupt training run. 
@@ -74,7 +99,185 @@ public class ParallelEvalMultiGamesMultiAgents
 	 */
 	public void startExperiment()
 	{
+		final AtomicInteger numCoresAvailable = new AtomicInteger(numCoresTotal);
+		final ExecutorService threadPool = Executors.newFixedThreadPool(numCoresTotal / numThreadsPerTrial, DaemonThreadFactory.INSTANCE);
 		
+		final long startTime = System.currentTimeMillis();
+		
+		try
+		{
+			for (final String jsonFile : jsonFiles)
+			{
+				// Let's try to start another batch of trials
+				final TrialsBatchToRun trialsBatch = TrialsBatchToRun.fromJson(jsonFile);
+				
+				// Load game for this batch
+				final Game game;
+				
+				if (trialsBatch.ruleset != null && !trialsBatch.ruleset.equals(""))
+					game = GameLoader.loadGameFromName(trialsBatch.gameName, trialsBatch.ruleset);
+				else
+					game = GameLoader.loadGameFromName(trialsBatch.gameName, new ArrayList<String>());	// TODO add support for options
+				
+				final int numPlayers = game.players().count();
+				
+				if (trialsBatch.agentStrings.length != numPlayers)
+				{
+					System.err.println
+					(
+						"Expected " + numPlayers + 
+						" agents, but received list of " + trialsBatch.agentStrings.length + 
+						" agents. Aborting set of games."
+					);
+					break;
+				}
+				
+				if (trialsBatch.gameLengthCap >= 0)
+					game.setMaxTurns(Math.min(trialsBatch.gameLengthCap, game.getMaxTurnLimit()));
+				
+				// Permutations of agents list, to rotate through
+				final List<TIntArrayList> aiListPermutations;
+				if (numPlayers <= 5)
+				{
+					// Compute all possible permutations of indices for the list of AIs
+					aiListPermutations = ListUtils.generatePermutations(
+							TIntArrayList.wrap(IntStream.range(0, numPlayers).toArray()));
+					
+					Collections.shuffle(aiListPermutations);
+				}
+				else
+				{
+					// Randomly generate some permutations of indices for the list of AIs
+					aiListPermutations = ListUtils.samplePermutations(
+							TIntArrayList.wrap(IntStream.range(0, numPlayers).toArray()), 120);
+				}
+				
+				{
+					// Do a warming up (just a few seconds anyway, so let's not worry about parallelisation)
+					final Trial trial = new Trial(game);
+					final Context context = new Context(game, trial);
+					long stopAt = 0L;
+					final long start = System.nanoTime();
+					final double abortAt = start + trialsBatch.warmingUpSecs * 1000000000.0;
+					while (stopAt < abortAt)
+					{
+						game.start(context);
+						game.playout(context, null, 1.0, null, -1, -1, ThreadLocalRandom.current());
+						stopAt = System.nanoTime();
+					}
+					System.gc();
+				}
+				
+				// Prepare results writing (ResultsSummary is thread-safe)
+				final List<String> agentStrings = new ArrayList<String>();
+				for (final String agentString : trialsBatch.agentStrings)
+				{
+					agentStrings.add(AIFactory.createAI(agentString).friendlyName());
+				}
+				final ResultsSummary resultsSummary = new ResultsSummary(game, agentStrings);
+				
+				for (int trialCounter = 0; trialCounter < trialsBatch.numTrials; ++trialCounter)
+				{
+					// Submit another job to the thread pool for this specific trial
+					final int thisTrialCounter = trialCounter;		// Need to make this variable final for inner class
+					
+					threadPool.submit
+					(
+						() -> 
+						{
+							try
+							{
+								numCoresAvailable.decrementAndGet();
+								
+								// Compute list of AIs to use for this trial (we rotate every trial)
+								final List<AI> currentAIList = new ArrayList<AI>(numPlayers);
+								final int currentAIsPermutation = thisTrialCounter % aiListPermutations.size();
+								
+								final TIntArrayList currentPlayersPermutation = aiListPermutations.get(currentAIsPermutation);
+								currentAIList.add(null); // 0 index not used
+
+								for (int i = 0; i < currentPlayersPermutation.size(); ++i)
+								{
+									currentAIList.add
+									(
+										AIFactory.createAI(trialsBatch.agentStrings[currentPlayersPermutation.getQuick(i) % numPlayers])
+									);
+								}
+
+								// Play a game
+								final Trial trial = new Trial(game);
+								final Context context = new Context(game, trial);
+								game.start(context);
+
+								for (int p = 1; p < currentAIList.size(); ++p)
+								{
+									currentAIList.get(p).initAI(game, p);
+								}
+								
+								final Model model = context.model();
+
+								while (!context.trial().over())
+								{
+									model.startNewStep
+									(
+										context, currentAIList, trialsBatch.thinkingTime, trialsBatch.iterationLimit, 
+										-1, 0.0
+									);
+								}
+
+								// Record results
+								if (context.trial().over())
+								{
+									final double[] utilities = RankUtils.agentUtilities(context);
+									final int numMovesPlayed = context.trial().numMoves() - context.trial().numInitialPlacementMoves();
+									final int[] agentPermutation = new int[currentPlayersPermutation.size() + 1];
+									currentPlayersPermutation.toArray(agentPermutation, 0, 1, currentPlayersPermutation.size());
+									
+									resultsSummary.recordResults(agentPermutation, utilities, numMovesPlayed);
+								}
+								
+								// Close AIs
+								for (int p = 1; p < currentAIList.size(); ++p)
+								{
+									currentAIList.get(p).closeAI();
+								}
+							}
+							catch (final Exception e)
+							{
+								e.printStackTrace();
+							}
+							finally
+							{
+								numCoresAvailable.incrementAndGet();
+							}
+						}
+					);
+				}
+				
+				// TODO write results
+
+				while (numCoresAvailable.get() <= 0)
+				{
+					// We could just continue creating jobs and submitting them to the pool, but then we would
+					// already be loading all those games into memory. We don't want to do that, so we'll wait
+					// with submitting more tasks until we actually have cores to run them.
+					Thread.sleep(10000L);
+				}
+			}
+			
+			// Shut down, but wait until everything that is still running is done
+			if (threadPool != null)
+			{
+				final long maxWallTimeMillis = maxWallTime * 60 * 1000L;
+				final long alreadyElapsedTime = System.currentTimeMillis() - startTime;
+				
+				threadPool.shutdown();
+				threadPool.awaitTermination(maxWallTimeMillis - alreadyElapsedTime, TimeUnit.MILLISECONDS);
+			}
+		}
+		catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	//-------------------------------------------------------------------------
@@ -88,18 +291,15 @@ public class ParallelEvalMultiGamesMultiAgents
 	public static class TrialsBatchToRun
 	{
 		
-		private final String gameName;
-		private final String ruleset;
-		private final int numTrials;
-		private final int gameLengthCap;
-		private final double thinkingTime;
-		private final int iterationLimit;
-		private final int warmingUpSecs;
-		private final String outDir;
-		private final String[] agentStrings;
-		private final int maxWallTime;		// in minutes
-		private final String outRedirect;
-		private final String errRedirect;
+		protected final String gameName;
+		protected final String ruleset;
+		protected final int numTrials;
+		protected final int gameLengthCap;
+		protected final double thinkingTime;
+		protected final int iterationLimit;
+		protected final int warmingUpSecs;
+		protected final String outDir;
+		protected final String[] agentStrings;
 		
 		/**
 		 * Constructor
@@ -113,15 +313,12 @@ public class ParallelEvalMultiGamesMultiAgents
 		 * @param warmingUpSecs
 		 * @param outDir
 		 * @param agentStrings
-		 * @param maxWallTime
-		 * @param outRedirect
-		 * @param errRedirect
 		 */
 		public TrialsBatchToRun
 		(
 			final String gameName, final String ruleset, final int numTrials, final int gameLengthCap, 
 			final double thinkingTime, final int iterationLimit, final int warmingUpSecs, final String outDir, 
-			final String[] agentStrings, final int maxWallTime, final String outRedirect, final String errRedirect
+			final String[] agentStrings
 		) 
 		{
 			this.gameName = gameName;
@@ -133,9 +330,6 @@ public class ParallelEvalMultiGamesMultiAgents
 			this.warmingUpSecs = warmingUpSecs;
 			this.outDir = outDir;
 			this.agentStrings = agentStrings;
-			this.maxWallTime = maxWallTime;
-			this.outRedirect = outRedirect;
-			this.errRedirect = errRedirect;
 		}
 		
 		public void toJson(final String jsonFilepath)
@@ -160,9 +354,6 @@ public class ParallelEvalMultiGamesMultiAgents
 				json.put("outDir", outDir);
 				final JSONArray agentStringsJsonArray = new JSONArray(Arrays.asList(agentStrings));
 				json.put("agentStrings", agentStringsJsonArray);
-				json.put("maxWallTime", maxWallTime);
-				json.put("outRedirect", outRedirect);
-				json.put("errRedirect", errRedirect);
 
 				final FileWriter fw = new FileWriter(file);
 				bw = new BufferedWriter(fw);
@@ -203,12 +394,9 @@ public class ParallelEvalMultiGamesMultiAgents
 				final String outDir = json.getString("outDir");
 				final JSONArray jArray = json.optJSONArray("agentStrings");
 				final String[] agentStrings = jArray.toList().toArray(new String[0]);
-				final int maxWallTime = json.getInt("maxWallTime");
-				final String outRedirect = json.getString("outRedirect");
-				final String errRedirect = json.getString("errRedirect");
 				
 				return new TrialsBatchToRun(gameName, ruleset, numTrials, gameLengthCap, thinkingTime, iterationLimit, 
-						warmingUpSecs, outDir, agentStrings, maxWallTime, outRedirect, errRedirect);
+						warmingUpSecs, outDir, agentStrings);
 
 			}
 			catch (final Exception e)
@@ -227,6 +415,7 @@ public class ParallelEvalMultiGamesMultiAgents
 	 * testing with elaborate setup through command-line args
 	 * @param args
 	 */
+	@SuppressWarnings("unchecked")
 	public static void main(final String[] args)
 	{
 		// Feature Set caching is safe in this main method
@@ -240,6 +429,37 @@ public class ParallelEvalMultiGamesMultiAgents
 					"Evaluate many agents in many games in parallel. Configuration of all experiments to be run should be in a JSON file."
 				);
 		
+		argParse.addOption(new ArgOption()
+				.withNames("--num-cores-total")
+				.help("Total number of cores we expect to be able to use for all jobs together.")
+				.withNumVals(1)
+				.withType(OptionTypes.Int)
+				.setRequired());
+		argParse.addOption(new ArgOption()
+				.withNames("--num-threads-per-trial")
+				.help("Number of threads to be used per individual trial (e.g., by AIs).")
+				.withNumVals(1)
+				.withType(OptionTypes.Int)
+				.setRequired());
+		
+		argParse.addOption(new ArgOption()
+				.withNames("--json-files")
+				.help("JSON files, each describing one batch of trials, which we should run in this job.")
+				.withNumVals("+")
+				.withType(OptionTypes.String)
+				.setRequired());
+		
+		argParse.addOption(new ArgOption()
+				.withNames("--useGUI")
+				.help("Whether to create a small GUI that can be used to "
+						+ "manually interrupt training run. False by default."));
+		argParse.addOption(new ArgOption()
+				.withNames("--max-wall-time")
+				.help("Max wall time in minutes (or -1 for no limit).")
+				.withDefault(Integer.valueOf(-1))
+				.withNumVals(1)
+				.withType(OptionTypes.Int));
+		
 		// Parse the args
 		if (!argParse.parseArguments(args))
 			return;
@@ -251,6 +471,10 @@ public class ParallelEvalMultiGamesMultiAgents
 					argParse.getValueBool("--useGUI"), 
 					argParse.getValueInt("--max-wall-time")
 				);
+		
+		experiment.numCoresTotal = argParse.getValueInt("--num-cores-total");
+		experiment.numThreadsPerTrial = argParse.getValueInt("--num-threads-per-trial");
+		experiment.jsonFiles = (List<String>) argParse.getValue("--json-files");
 		
 		experiment.startExperiment();
 	}
