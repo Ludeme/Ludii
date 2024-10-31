7b37c9843254ce0b41fd69aaac7c271a1e6ee7e8
ericp <ericp@192.168.0.65>
2024-10-31 09:33:21 +0100

    UCT trials

--- Mining/src/utils/trials/CreateLeMaitre4ClusterTrialsScript.java
+++ Mining/src/utils/trials/CreateLeMaitre4ClusterTrialsScript.java
@@ -49,7 +49,7 @@ public class CreateLeMaitre4ClusterTrialsScript
 		final int numPlayout = 100;
 		final int maxMove = 5000; // Constants.DEFAULT_MOVES_LIMIT;
 		final int thinkingTime = 1;
-		final String agentName = "Random"; // Can be "UCT",  "Alpha-Beta", "Alpha-Beta-UCT", "AB-Odd-Even", or "Random"
+		final String agentName = "UCT"; // Can be "UCT",  "Alpha-Beta", "Alpha-Beta-UCT", "AB-Odd-Even", or "Random"
 		final String clusterLogin = "epiette";
 		final String mainScriptName = "GenTrials.sh";
 		final int numRulesetsPerBatch = 8; // 48
