ebec1a442601dac9c5cf24de930eaa19f8d6070c
ericp <ericp@192.168.0.65>
2025-02-20 14:28:01 +0100

    Preparation for 1.3.14 new Ludii version

--- Common/src/main/Constants.java
+++ Common/src/main/Constants.java
@@ -11,10 +11,10 @@ public final class Constants
 	// Admin
 	
 	/** Version number of the Ludii's grammar/ludeme-based language. */
-	public static final String LUDEME_VERSION = "1.3.13";
+	public static final String LUDEME_VERSION = "1.3.14";
 
 	/** Date last modified. */
-	public static final String DATE = "10/07/2024";
+	public static final String DATE = "20/02/2025";
 	
 	/** lud-path for default game to load (on initial launch, when prefs/trial loading fails, etc.) */
 	public static final String DEFAULT_GAME_PATH = "/lud/board/war/replacement/eliminate/all/Surakarta.lud";
